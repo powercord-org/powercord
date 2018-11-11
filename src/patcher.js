773536d5d3a9d74448c755b2202397f0a9cedc58
aetheryx <zainalih3c@gmail.com>
2018-11-11 18:26:49 +0100

    add patcher

--- src/patcher.js
+++ src/patcher.js
@@ -1,1 +1,36 @@
-console.log('no');
+// Loosely based on ED/DI injector
+
+const { BrowserWindow, app } = require('electron');
+const { join, dirname } = require('path');
+
+class PatchedBrowserWindow extends BrowserWindow {
+  constructor (opts) {
+    if (opts.webPreferences && opts.webPreferences.preload) {
+      process.env.originalPreload = opts.webPreferences.preload
+      opts.webPreferences.preload = join(__dirname, 'preload');
+    }
+
+    return new BrowserWindow(opts);
+  }
+}
+
+app.on('ready', () => {
+  const electronCacheEntry = require.cache[require.resolve('electron')];
+  Object.defineProperty(electronCacheEntry, 'exports', {
+    value: {
+      ...electronCacheEntry.exports
+    }
+  });
+  electronCacheEntry.exports.BrowserWindow = PatchedBrowserWindow;
+
+  const discordPath = join(dirname(require.main.filename), '..', 'app.asar')
+  
+  require('module')
+    ._load(
+      join(
+        discordPath,
+        require(join(discordPath, 'package.json')).main
+      ),
+      null, true
+    );
+});
