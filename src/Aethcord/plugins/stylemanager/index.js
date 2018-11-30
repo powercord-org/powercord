59bb7c71b0ff03efd12b99b4faecba0346c82ca1
nycex <nycexyt@gmail.com>
2018-11-30 17:30:55 +0100

    use aetheryx' watch wrapper

--- src/Aethcord/plugins/stylemanager/index.js
+++ src/Aethcord/plugins/stylemanager/index.js
@@ -1,39 +1,40 @@
 const Plugin = require('ac/Plugin');
+const { watch } = require('ac/util');
+
 const { readdir, readFile } = require('fs').promises;
-const { watch } = require('fs');
-
-const loadCSS = async (path) => {
-  const dir = await readdir(path);
-  for (const filename of dir) {
-    const file = await readFile(`${path}/${filename}`);
-
-    const style = document.createElement('style');
-    style.innerHTML = file.toString();
-    style.id = `aethcord-css-${filename.split('.').shift()}`;
-    document.head.appendChild(style);
-  }
-};
-
-const clearCSS = async () => {
-  document.querySelectorAll('[id^="aethcord-css-"]').forEach((item) => {
-    item.parentNode.removeChild(item);
-  });
-};
 
 module.exports = class StyleManager extends Plugin {
   constructor () {
     super({
       stage: 2
     });
+
+    this.styleDir = `${__dirname}/styles`;
+  }
+
+  async update (path) {
+    if (path.endsWith('~')) {
+      path = path.slice(0, -1);
+    }
+    const id = path.split('.').shift();
+    const content = await readFile(`${this.styleDir}/${path}`);
+    document.getElementById(`aethcord-css-${id}`).innerHTML = content.toString();
+  }
+
+  async loadInitialCSS () {
+    const dir = await readdir(this.styleDir);
+    for (const filename of dir) {
+      const file = await readFile(`${this.styleDir}/${filename}`);
+
+      const style = document.createElement('style');
+      style.innerHTML = file.toString();
+      style.id = `aethcord-css-${filename.split('.').shift()}`;
+      document.head.appendChild(style);
+    }
   }
 
   async start () {
-    const path = `${__dirname}/styles`;
-    loadCSS(path);
-    watch(path, { encoding: 'utf-8' }, () => {
-      clearCSS();
-      loadCSS(path);
-    });
+    this.loadInitialCSS();
+    watch(this.styleDir, this.update.bind(this));
   }
 };
-
