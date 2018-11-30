c5b2410892390279c8eb38c103516a4566d6169d
nycex <nycexyt@gmail.com>
2018-11-30 17:35:11 +0100

    implement css hotreload (#11)
    * implement css hotreload
    
    * use aetheryx' watch wrapper
    
    * add aetheryx' wrapper actually

--- src/Aethcord/plugins/stylemanager/index.js
+++ src/Aethcord/plugins/stylemanager/index.js
@@ -1,4 +1,6 @@
 const Plugin = require('ac/Plugin');
+const { watch } = require('ac/util');
+
 const { readdir, readFile } = require('fs').promises;
 
 module.exports = class StyleManager extends Plugin {
@@ -6,17 +8,33 @@ module.exports = class StyleManager extends Plugin {
     super({
       stage: 2
     });
+
+    this.styleDir = `${__dirname}/styles`;
   }
 
-  async start () {
-    const dir = await readdir(`${__dirname}/styles`);
+  async update (path) {
+    if (path.endsWith('~')) {
+      path = path.slice(0, -1);
+    }
+    const id = path.split('.').shift();
+    const content = await readFile(`${this.styleDir}/${path}`);
+    document.getElementById(`aethcord-css-${id}`).innerHTML = content.toString();
+  }
+
+  async loadInitialCSS () {
+    const dir = await readdir(this.styleDir);
     for (const filename of dir) {
-      const file = await readFile(`${__dirname}/styles/${filename}`);
+      const file = await readFile(`${this.styleDir}/${filename}`);
 
       const style = document.createElement('style');
       style.innerHTML = file.toString();
       style.id = `aethcord-css-${filename.split('.').shift()}`;
       document.head.appendChild(style);
     }
+  }
+
+  async start () {
+    this.loadInitialCSS();
+    watch(this.styleDir, this.update.bind(this));
   }
 };
