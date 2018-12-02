cca94f3216793c81d1f04f31c4e312255bbd68d5
Bowser65 <contact.bowser65@gmail.com>
2018-12-02 16:29:25 +0100

    SCSS support (closes #14)

--- src/Aethcord/plugins/stylemanager/index.js
+++ src/Aethcord/plugins/stylemanager/index.js
@@ -1,6 +1,6 @@
 const Plugin = require('ac/Plugin');
 const { watch } = require('ac/util');
-// const { render } = require('node-sass');
+const { renderSync } = require('sass');
 const { readdir, readFile } = require('fs').promises;
 
 module.exports = class StyleManager extends Plugin {
@@ -9,42 +9,49 @@ module.exports = class StyleManager extends Plugin {
       stage: 2
     });
 
-    this.styleDir = `${__dirname}/styles`;
+    this.styleDir = `${__dirname.replace(/\\/g, '/')}/styles`;
+    this.trackedFiles = [];
   }
 
   async update (path) {
     if (!path.match(/\.s?css$/)) {
       return;
     }
-    const id = path.split(/[\\|/]/).pop().split('.').shift();
-    const styleElement = document.getElementById(`aethcord-css-${id}`);
-    if (styleElement) {
-      styleElement.innerHTML = await this.readFile(path);
+    const file = path.replace(/\\/g, '/').replace(`${this.styleDir}/`, '');
+    const match = this.trackedFiles.filter(f => f.file === file || f.includes.includes(file));
+    if (match.length !== 0) {
+      const id = match[0].file.split('.').shift();
+      document.getElementById(`aethcord-css-${id}`).innerHTML = await this.readFile(match[0].file);
     }
   }
 
   async loadInitialCSS () {
     const dir = await readdir(this.styleDir);
     for (const filename of dir) {
-      if (!filename.match(/\.s?css$/)) {
+      if (!filename.match(/\.s?css$/) || filename.match(/^_.*\.scss$/)) {
         continue;
       }
 
+      this.trackedFiles.push({
+        file: filename,
+        includes: []
+      });
       const style = document.createElement('style');
-      style.innerHTML = await this.readFile(`${this.styleDir}/${filename}`);
+      style.innerHTML = await this.readFile(filename);
       style.id = `aethcord-css-${filename.split('.').shift()}`;
       document.head.appendChild(style);
     }
   }
 
-  async readFile (path) {
-    const file = await readFile(path);
-    if (path.endsWith('scss')) {
-      /*
-       * return render({
-       *   data: file.toString()
-       * });
-       */
+  async readFile (filename) {
+    const file = await readFile(`${this.styleDir}/${filename}`);
+    if (filename.endsWith('scss')) {
+      const result = renderSync({
+        data: file.toString(),
+        includePaths: [ this.styleDir ]
+      });
+      this.trackedFiles.filter(f => f.file === filename)[0].includes = result.stats.includedFiles.map(e => e.split('/').pop());
+      return result.css.toString();
     }
     return file.toString();
   }
