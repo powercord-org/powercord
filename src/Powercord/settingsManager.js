ff19c9377f31836153653abb210cf2df2946db8c
aetheryx <zainalih3c@gmail.com>
2018-12-30 16:05:15 +0100

    add dot syntax support

--- src/Powercord/settingsManager.js
+++ src/Powercord/settingsManager.js
@@ -1,33 +1,60 @@
 const { resolve } = require('path');
 const { writeFile } = require('fs');
 
+const configPath = resolve(__dirname, '..', '..', 'config.json');
+
 let config;
 try {
-  config = require('../../config.json');
+  config = require(configPath);
 } catch (e) {
   config = {};
 }
 
 module.exports = class SettingsManager {
   constructor (category) {
-    this.category = category || 'general';
+    if (!category) {
+      throw new TypeError('Missing SettingsManager category name');
+    }
+
+    this.category = category;
     this.config = config[this.category] || {};
   }
 
-  get (key, defaultValue) {
-    const value = this.config[key];
-    return (value === void 0 || value === null)
+  get (nodePath, defaultValue) {
+    const nodePaths = nodePath.split('.');
+    let currentNode = this.config;
+
+    for (const fragment of nodePaths) {
+      currentNode = currentNode[fragment];
+    }
+
+    return (currentNode === void 0 || currentNode === null)
       ? defaultValue
-      : value;
+      : currentNode;
   }
 
-  set (key, value) {
-    this.config[key] = value;
+  set (nodePath, value) {
+    const nodePaths = nodePath.split('.');
+    let currentNode = this.config;
+
+    for (const fragment of nodePaths) {
+      if (nodePaths.indexOf(fragment) === nodePaths.length - 1) {
+        currentNode[fragment] = value;
+      } else if (!currentNode[fragment]) {
+        currentNode[fragment] = {};
+      }
+
+      currentNode = currentNode[fragment];
+    }
+
     this._save();
   }
 
   _save () {
-    const cfg = Object.assign({}, config, { [this.category]: this.config });
-    writeFile(resolve(__dirname, '..', '..', 'config.json'), JSON.stringify(cfg, null, 2), () => null);
+    Object.assign(config, {
+      [this.category]: this.config
+    });
+
+    writeFile(configPath, JSON.stringify(config, null, 2), () => {});
   }
 };
