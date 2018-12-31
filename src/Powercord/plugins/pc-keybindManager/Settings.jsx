0d4d0a90afa8c3e6b9f1c3ee1b8aa8e6051e74e2
Bowser65 <contact.bowser65@gmail.com>
2018-12-31 14:54:02 +0100

    fine, no stateless then

--- src/Powercord/plugins/pc-keybindManager/Settings.jsx
+++ src/Powercord/plugins/pc-keybindManager/Settings.jsx
@@ -1,16 +1,22 @@
-const { React } = require('powercord/webpack'); // eslint-disable-line no-unused-vars
+const { React } = require('powercord/webpack');
 const { KeybindRecorder } = require('powercord/components/settings');
 
-module.exports = () => <div>
-  {this.props.keybinds.map(keybind => <KeybindRecorder
-    key={keybind.id}
-    value={keybind.keybind}
-    note={keybind.description}
-    onChange={(v) => this.props.onChange(keybind.id, v)}
-    onReset={() => {
-      this.props.onChange(keybind.id, null);
-      this.forceUpdate();
-    }}
-    onRecord={() => this.props.onRecord(keybind.id)}
-  >{keybind.name}</KeybindRecorder>)}
-</div>;
+module.exports = class Settings extends React.Component {
+  render () {
+    return (
+      <div>
+        {this.props.keybinds.map(keybind => <KeybindRecorder
+          key={keybind.id}
+          value={keybind.keybind}
+          note={keybind.description}
+          onChange={(v) => this.props.onChange(keybind.id, v)}
+          onReset={() => {
+            this.props.onChange(keybind.id, null);
+            this.forceUpdate();
+          }}
+          onRecord={() => this.props.onRecord(keybind.id)}
+        >{keybind.name}</KeybindRecorder>)}
+      </div>
+    );
+  }
+};
