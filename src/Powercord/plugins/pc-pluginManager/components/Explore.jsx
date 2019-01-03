07e29e102dcef9565c86d1321e8eab53d075c511
Bowser65 <contact.bowser65@gmail.com>
2019-01-03 17:41:40 +0100

    petite faute d'orthographe parce que l'anglais c'est pas facile

--- src/Powercord/plugins/pc-pluginManager/components/Explore.jsx
+++ src/Powercord/plugins/pc-pluginManager/components/Explore.jsx
@@ -4,7 +4,7 @@ const { Button } = require('powercord/components');
 module.exports = class Marketplace extends React.Component {
   render () {
     return <div className='powercord-plugins'>
-      <div className='powercord-plugins-wip'>This part of Powercord is a WIP. Except unavailable features and crashes here</div>
+      <div className='powercord-plugins-wip'>This part of Powercord is a WIP. Expect unavailable features and crashes here</div>
       <div className='powercord-plugins-header'>
         <h3>Explore plugins</h3>
         <Button onClick={() => this.props.goToInstalled()}>Installed Plugins</Button>
