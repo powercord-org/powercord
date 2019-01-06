4db8689ce2a8f98d78b6b818bad1a595ab4083e1
Austin Melms <austin@melmsie.com>
2019-01-06 00:29:48 -0500

    Mock Plugin

---
+++ src/Powercord/plugins/pc-mock/index.js
@@ -0,0 +1,18 @@
+const Plugin = require('powercord/Plugin');
+
+module.exports = class Mock extends Plugin {
+  start () {
+    powercord
+      .pluginManager
+      .get('pc-commands')
+      .register(
+        'mock',
+        'Mock a user...',
+        '{c} [ text to mock ]',
+        (args) => ({
+          send: true,
+          result: args.join(' ').split('').map((c, i) => i % 2 ? c.toUpperCase() : c).join('')
+        })
+      );
+  }
+};
