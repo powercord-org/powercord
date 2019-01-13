a8c6f1a9c788a1ed6fad70731e2edcc47c892d20
Joakim <21Joakim@users.noreply.github.com>
2019-01-14 00:13:53 +0100

    Bug fix and multiple emote support
    Added support for finding multiple emotes at the same time. Fixed (I think) a bug which was caused by an emote not requiring colons

--- src/Powercord/plugins/pc-emojiUtility/index.js
+++ src/Powercord/plugins/pc-emojiUtility/index.js
@@ -7,6 +7,16 @@ module.exports = class EmojiUtility extends Plugin {
     return /^<a?:([a-zA-Z0-9_]+):([0-9]+)>$/;
   }
 
+  getGuildUrl (guild) {
+    let selectedChannel = channels.getSelectedChannelState()[guild.id];
+    if (!selectedChannel) {
+      /* I am not sure if it can get here but just to be sure */
+      selectedChannel = guild.systemChannelId;
+    }
+
+    return APP_URL_PREFIX + Routes.CHANNEL(guild.id, selectedChannel); // eslint-disable-line new-cap
+  }
+
   start () {
     powercord
       .pluginManager
@@ -16,8 +26,7 @@ module.exports = class EmojiUtility extends Plugin {
         'Find the server an emote is from',
         '{c} [emote]',
         (args) => {
-          const argument = args.join(' ');
-          if (argument.length === 0) {
+          if (args.length === 0) {
             return {
               send: false,
               result: {
@@ -28,49 +37,80 @@ module.exports = class EmojiUtility extends Plugin {
             };
           }
 
-          const matcher = argument.match(this.getEmojiRegex());
-          if (matcher) {
-            const emojis = Object.values(emojiStore.getGuilds()).flatMap(r => r.emojis);
-            const emoji = emojis.find(e => e.id === matcher[2]);
+          const emojis = Object.values(emojiStore.getGuilds()).flatMap(r => r.emojis);
 
-            if (emoji) {
-              const guild = getModule([ 'getGuild' ]).getGuild(emoji.guildId);
+          const foundEmojis = [];
+          const notFoundEmojis = [];
 
-              let selectedChannel = channels.getSelectedChannelState()[guild.id];
-              if (!selectedChannel) {
-                /* I am not sure if it can get here but just to be sure */
-                selectedChannel = guild.systemChannelId;
+          for (const argument of args) {
+            const matcher = argument.match(this.getEmojiRegex());
+            if (matcher) {
+              const emoji = emojis.find(e => e.id === matcher[2]);
+
+              if (emoji) {
+                const guild = getModule([ 'getGuild' ]).getGuild(emoji.guildId);
+
+                if (args.length === 1) {
+                  return {
+                    send: false,
+                    result: {
+                      type: 'rich',
+                      description: `${argument} is from **[${guild.name}](${this.getGuildUrl(guild)})**`,
+                      color: 65280,
+                      footer: {
+                        text: `Guild: ${guild.id}`
+                      }
+                    }
+                  };
+                }
+
+                foundEmojis.push({
+                  name: argument,
+                  emoji,
+                  guild
+                });
+
+                continue;
               }
 
-              const url = APP_URL_PREFIX + Routes.CHANNEL(guild.id, selectedChannel); // eslint-disable-line new-cap
+              if (args.length === 1) {
+                return {
+                  send: false,
+                  result: {
+                    type: 'rich',
+                    description: `Could not find emote ${argument}`,
+                    color: 16711680
+                  }
+                };
+              }
+            }
 
+            if (args.length === 1) {
               return {
                 send: false,
                 result: {
                   type: 'rich',
-                  description: `${argument} is from **[${guild.name}](${url})**`,
-                  color: 65280,
-                  footer: {
-                    text: `Guild: ${guild.id}`
-                  }
+                  description: `**${argument}** is not a custom emote`,
+                  color: 16711680
                 }
               };
             }
-            return {
-              send: false,
-              result: {
-                type: 'rich',
-                description: `Could not find emote ${argument}`,
-                color: 16711680
-              }
-            };
+
+            notFoundEmojis.push(argument);
           }
+
+          let description = '';
+          for (const found of foundEmojis) {
+            description += `${found.name} is from **[${found.guild.name}](${this.getGuildUrl(found.guild)})**\n`;
+          }
+
           return {
             send: false,
             result: {
               type: 'rich',
-              description: `**${argument}** is not a custom emote`,
-              color: 16711680
+              description: description.trim(),
+              color: 65280,
+              footer: notFoundEmojis.length > 0 ? { text: `${notFoundEmojis.length} of the provided arguments ${notFoundEmojis.length === 1 ? 'is not a custom emote' : 'are not custom emotes'}` } : null
             }
           };
         }
@@ -100,7 +140,7 @@ module.exports = class EmojiUtility extends Plugin {
 
           const foundEmojis = emojis.filter(emoji => emoji.name.toLowerCase().includes(argument));
           if (foundEmojis.length > 0) {
-            const emojisAsString = foundEmojis.map(emoji => `<${(emoji.animated ? 'a' : '') + emoji.allNamesString + emoji.id}>`).join('');
+            const emojisAsString = foundEmojis.map(emoji => `<${(emoji.animated ? 'a' : '')}:${emoji.name}:${emoji.id}>`).join('');
             if (emojisAsString.length > 2000) {
               return {
                 send: false,
