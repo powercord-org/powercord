1df7a1fa023484d95049b16b737b93d7ab7fc40b
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-26 21:53:14 +0000

    volume command

---
+++ src/Powercord/plugins/pc-spotify/commands/volume.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'volume',
+  description: 'Change Spotify volume',
+  usage: '/volume <number between 1-100>',
+
+  func (SpotifyPlayer, args) {
+    return SpotifyPlayer.setVolume(args);
+  }
+};
