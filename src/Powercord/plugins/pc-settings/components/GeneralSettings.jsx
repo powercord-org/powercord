be7dd231a42c3eb3c282c6e829f1656a6d993a2b
Bowser65 <contact.bowser65@gmail.com>
2019-02-03 22:29:52 +0100

    New settings category component

--- src/Powercord/plugins/pc-settings/components/GeneralSettings.jsx
+++ src/Powercord/plugins/pc-settings/components/GeneralSettings.jsx
@@ -1,6 +1,6 @@
 const { React } = require('powercord/webpack');
 const { open: openModal, close: closeModal } = require('powercord/modal');
-const { TextInput, SwitchItem } = require('powercord/components/settings');
+const { TextInput, SwitchItem, Category } = require('powercord/components/settings');
 
 const PassphraseModal = require('./PassphraseModal.jsx');
 const Account = require('./PowercordAccount');
@@ -52,55 +52,51 @@ module.exports = class GeneralSettings extends React.Component {
           Settings Sync
         </SwitchItem>
 
-        <SwitchItem
-          note={
+        <Category
+          name='Advanced Settings'
+          description={
             <span>Exercise caution changing anything in this category if you don't know what you're doing. <b>Seriously.</b></span>
           }
-          value={settings.advancedSettings}
+          opened={settings.advancedSettings}
           onChange={() => this._set('advancedSettings')}
         >
-          Advanced Settings
-        </SwitchItem>
-        {settings.advancedSettings && (
-          <div>
-            <TextInput
-              value={settings.backendURL}
-              required={true}
-              onChange={(e) => this._set('backendURL', e, 'https://powercord.xyz')}
-              note='URL used for Spotify linking, plugin management and other internal functions'
-            >
-              Backend URL
-            </TextInput>
+          <TextInput
+            value={settings.backendURL}
+            required={true}
+            onChange={(e) => this._set('backendURL', e, 'https://powercord.xyz')}
+            note='URL used for Spotify linking, plugin management and other internal functions'
+          >
+            Backend URL
+          </TextInput>
 
-            <SwitchItem
-              note='Should Powercord open overlay devtools when it gets injected? (useful for developing themes)'
-              value={settings.openOverlayDevTools}
-              onChange={() => this._set('openOverlayDevTools')}
-            >
-              Overlay DevTools
-            </SwitchItem>
+          <SwitchItem
+            note='Should Powercord open overlay devtools when it gets injected? (useful for developing themes)'
+            value={settings.openOverlayDevTools}
+            onChange={() => this._set('openOverlayDevTools')}
+          >
+            Overlay DevTools
+          </SwitchItem>
 
-            <SwitchItem
-              note='Prevents Discord from removing your token from localStorage, reducing the numbers of unwanted logouts.'
-              value={settings.hideToken}
-              onChange={() => this._set('hideToken')}
-            >
-              Keep token stored
-            </SwitchItem>
+          <SwitchItem
+            note='Prevents Discord from removing your token from localStorage, reducing the numbers of unwanted logouts.'
+            value={settings.hideToken}
+            onChange={() => this._set('hideToken')}
+          >
+            Keep token stored
+          </SwitchItem>
 
-            <SwitchItem
-              note={
-                <span><b style={{ color: 'rgb(240, 71, 71)' }}>WARNING:</b> Enabling this gives you access to features that can be <b>detected by Discord</b> and may result in an <b
-                  style={{ color: 'rgb(240, 71, 71)' }}>account termination</b>.
+          <SwitchItem
+            note={
+              <span><b style={{ color: 'rgb(240, 71, 71)' }}>WARNING:</b> Enabling this gives you access to features that can be <b>detected by Discord</b> and may result in an <b
+                style={{ color: 'rgb(240, 71, 71)' }}>account termination</b>.
                   Powercord is <b>not responsible</b> for what you do with this feature. Leave it disabled if you are unsure.</span>
-              }
-              value={settings.experiments}
-              onChange={() => this._set('experiments')}
-            >
-              Enable Discord Experiments
-            </SwitchItem>
-          </div>
-        )}
+            }
+            value={settings.experiments}
+            onChange={() => this._set('experiments')}
+          >
+            Enable Discord Experiments
+          </SwitchItem>
+        </Category>
       </div>
     );
   }
