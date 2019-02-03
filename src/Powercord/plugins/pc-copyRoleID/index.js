a317185c4b515fd6aac52306082f925a82748156
Lars <23457286+MrLar@users.noreply.github.com>
2019-02-03 21:54:55 +0100

    Add context menu to roles (see todo in file) (#92)

---
+++ src/Powercord/plugins/pc-copyRoleID/index.js
@@ -0,0 +1,29 @@
+const Plugin = require('powercord/Plugin');
+const { contextMenu, getModuleByDisplayName, React } = require('powercord/webpack');
+const { ContextMenu } = require('powercord/components');
+const { inject } = require('powercord/injector');
+const { clipboard } = require('electron');
+
+// todo figure out how to do the same on UserPopOuts. Discussed with Aeth but seems to be a bit more complicated
+module.exports = class CopyRoleID extends Plugin {
+  start () {
+    const GuildRole = getModuleByDisplayName('GuildRole');
+    inject('pc-guildRole', GuildRole.prototype, 'render', (args, res) => { // eslint-disable-line func-names
+      res.props.children.props.onContextMenu = (e) => {
+        const { pageX, pageY } = e;
+        contextMenu.openContextMenu(e, () =>
+          React.createElement(ContextMenu, {
+            pageX,
+            pageY,
+            itemGroups: [ [ {
+              type: 'button',
+              name: 'Copy ID',
+              onClick: () => clipboard.writeText(res.props.id)
+            } ] ]
+          })
+        );
+      };
+      return res;
+    });
+  }
+};
