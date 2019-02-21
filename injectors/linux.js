6115126de943ff4b2c2df4b72fe524f673a0be32
intrnl <intrnl28+github@gmail.com>
2019-02-21 22:28:55 +0700

    check for multiple path
    don't just check for `/opt`, some discord installation will install to `/usr/share` and potentially others.

--- injectors/linux.js
+++ injectors/linux.js
@@ -1,2 +1,23 @@
-exports.getAppDir = async () =>
-  '/opt/discord-canary/resources/app';
+/* eslint-disable multiline-comment-style */
+
+const { existsSync } = require('fs');
+const { join } = require('path');
+
+const paths = [
+  '/usr/share/discord-canary',
+  '/opt/discord-canary'
+
+  // '/snap/discord/current/usr/share/discord',
+  // '/var/lib/snapd/snap/discord/current/usr/share/discord',
+];
+
+exports.getAppDir = async () => {
+  const discordPath = paths
+    .filter(path => existsSync(path))[0];
+
+  return join(
+    discordPath,
+    'resources',
+    'app',
+  );
+};
