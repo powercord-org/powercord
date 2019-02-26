e9208a2b25b16f8947f0182675b99937771e7e53
intrnl <intrnl28+github@gmail.com>
2019-02-26 22:02:40 +0700

    remove commented path

--- injectors/linux.js
+++ injectors/linux.js
@@ -1,14 +1,9 @@
-/* eslint-disable multiline-comment-style */
-
 const { existsSync } = require('fs');
 const { join } = require('path');
 
 const paths = [
   '/usr/share/discord-canary',
   '/opt/discord-canary'
-
-  // '/snap/discord/current/usr/share/discord',
-  // '/var/lib/snapd/snap/discord/current/usr/share/discord',
 ];
 
 exports.getAppDir = async () => {
