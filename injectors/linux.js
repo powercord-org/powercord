43d119c0f264cf9bf5aef153b1e22df72b0e58b4
intrnl <intrnl28+github@gmail.com>
2019-02-26 23:12:50 +0700

    Check for multiple path in Linux injector (#106)
    * check for multiple path
    
    don't just check for `/opt`, some discord installation will install to `/usr/share` and potentially others.
    
    * remove commented path
    
    * use find instead of filter

--- injectors/linux.js
+++ injectors/linux.js
@@ -1,2 +1,18 @@
-exports.getAppDir = async () =>
-  '/opt/discord-canary/resources/app';
+const { existsSync } = require('fs');
+const { join } = require('path');
+
+const paths = [
+  '/usr/share/discord-canary',
+  '/opt/discord-canary'
+];
+
+exports.getAppDir = async () => {
+  const discordPath = paths
+    .find(path => existsSync(path));
+
+  return join(
+    discordPath,
+    'resources',
+    'app',
+  );
+};
