16548a53dd10a2d12fa476d85bea90d2ea2628dc
Bowser65 <contact.bowser65@gmail.com>
2019-02-27 15:16:01 +0100

    No add to playlist for u

--- src/Powercord/plugins/pc-spotify/Modal/Modal.jsx
+++ src/Powercord/plugins/pc-spotify/Modal/Modal.jsx
@@ -267,13 +267,13 @@ module.exports = class Modal extends React.Component {
                 />
               </Tooltip>
 
-              <Tooltip text="Save to Playlist" position="top">
+              {powercord.account && powercord.account.spotify && <Tooltip text="Save to Playlist" position="top">
                 <button
                   style={{ color: '#fff' }}
                   className='iconButtonDefault-2cKx7- iconButton-3V4WS5 button-2b6hmh small--aHOfS fas fa-plus-circle'
                   onClick={() => powercord.pluginManager.get('pc-spotify').openPlaylistModal(currentItem.uri)}
                 />
-              </Tooltip>
+              </Tooltip>}
             </div>}
           </SeekBar>
         </div>
