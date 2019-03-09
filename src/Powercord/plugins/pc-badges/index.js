e00690cb1babc6dee3edaf2543d8b9d14584ad1c
Bowser65 <contact.bowser65@gmail.com>
2019-03-09 21:56:26 +0100

    add a fancy badge to the guild

--- src/Powercord/plugins/pc-badges/index.js
+++ src/Powercord/plugins/pc-badges/index.js
@@ -1,6 +1,8 @@
 const { resolve } = require('path');
 const Plugin = require('powercord/Plugin');
-const { injectRecursive, uninject } = require('powercord/injector');
+const { GUILD_ID } = require('powercord/constants');
+const { Tooltip } = require('powercord/components');
+const { inject, injectRecursive, uninject } = require('powercord/injector');
 const { React, getModuleByDisplayName } = require('powercord/webpack');
 
 const BadgesComponent = require('./Badges.jsx');
@@ -8,18 +10,46 @@ const BadgesComponent = require('./Badges.jsx');
 module.exports = class Badges extends Plugin {
   start () {
     this.loadCSS(resolve(__dirname, 'style.scss'));
+    this._patchGuildHeaders();
     this._patchUserComponent();
   }
 
   unload () {
     this.unloadCSS();
-    uninject('pc-badges');
+    uninject('pc-badges-users');
+    uninject('pc-badges-guilds');
+  }
+
+  async _patchGuildHeaders () {
+    const GuildHeader = await getModuleByDisplayName('GuildHeader');
+    inject('pc-badges-guilds', GuildHeader.prototype, 'render', function (args, res) {
+      if (this.props.guild.id === GUILD_ID) {
+        res.props.children.props.children.props.children.unshift(
+          React.createElement(Tooltip,
+            {
+              text: 'Official',
+              position: 'bottom'
+            },
+            React.createElement('img',
+              {
+                src: 'https://powercord.xyz/assets/logo.svg',
+                style: {
+                  transform: 'rotate(45deg)',
+                  width: 20,
+                  height: 20,
+                  marginRight: 5
+                }
+              }))
+        );
+      }
+      return res;
+    });
   }
 
   async _patchUserComponent () {
     const UserProfile = await getModuleByDisplayName('UserProfile');
 
-    injectRecursive('pc-badges', UserProfile, [
+    injectRecursive('pc-badges-users', UserProfile, [
       { path: [ 'prototype' ] },
       { path: [ 'type' ] },
       { path: [ 'type', 'prototype' ] },
