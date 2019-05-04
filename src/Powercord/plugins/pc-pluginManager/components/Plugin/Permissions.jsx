e685403fcfe516579fd58c071a89faf52a212ad1
Bowser65 <contact.bowser65@gmail.com>
2019-05-04 13:05:22 +0200

    Add ext_api permission (finally lol)

--- src/Powercord/plugins/pc-pluginManager/components/Plugin/Permissions.jsx
+++ src/Powercord/plugins/pc-pluginManager/components/Plugin/Permissions.jsx
@@ -1,9 +1,10 @@
 const { React } = require('powercord/webpack');
-const { Icons: { Key, Keyboard, Security } } = require('powercord/components');
+const { Icons: { Key, Keyboard, Security, ImportExport } } = require('powercord/components');
 
 module.exports = ({ permissions }) =>
   <div className='powercord-plugin-permissions'>
     <div className='item'><Key/> This plugin will be able to:</div>
     {permissions.includes('keypresses') && <div className='item'><Keyboard/> Listen to keypresses even while Discord is not focused</div>}
     {permissions.includes('use_eud') && <div className='item'><Security/> Collect and use your data as soon as the plugin is installed</div>}
+    {permissions.includes('ext_api') && <div className='item'><ImportExport/> Perform requests to other remote services</div>}
   </div>;
