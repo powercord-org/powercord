2f0329994faf27029ca2230202af3a3e821ea8bc
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2019-08-11 11:49:08 -0500

    Added autocomplete

--- src/Powercord/plugins/pc-commands/commands/disable.js
+++ src/Powercord/plugins/pc-commands/commands/disable.js
@@ -17,9 +17,30 @@ module.exports = {
             result = 'You do not have that plugin installed'
         }
 
+        
         return { 
             send: false,
             result
         }
+        
+    },
+    autocompleteFunc (args) {
+        const plugins = powercord.pluginManager.getPlugins()
+            .sort((a, b) => a < b ? -1 : 1 || 0)
+            .map(plugin => powercord.pluginManager.plugins.get(plugin));
+        if (args.length > 1) {
+            return false;
+        }
+        return {
+            commands: plugins
+                .filter(plugin => plugin.pluginID !== 'pc-commands' &&
+                    plugin.pluginID.includes(args[0].toLowerCase()))
+                .map(plugin => ({
+                    command: plugin.pluginID,
+                    description: plugin.manifest.description
+                }))
+                .slice(0, 10),
+            header: 'powercord plugin list'
+        };
     }
 }
