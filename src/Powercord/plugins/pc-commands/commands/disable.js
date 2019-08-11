6ea11b8e52f1f4e68a55293066480824ec1c5411
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2019-08-11 11:56:20 -0500

    Small fix
    Forgot to remove the pc-commands check

--- src/Powercord/plugins/pc-commands/commands/disable.js
+++ src/Powercord/plugins/pc-commands/commands/disable.js
@@ -1,14 +1,12 @@
 module.exports = {
     command: 'disable',
     description: 'Disable a plugin',
-    usage: '{c} [ Plugin Name ]',
+    usage: '{c} [ PluginID ]',
     aliases: [],
     func: (args) => {
         let result;
         if(powercord.pluginManager.plugins.has(args[0])) {
-            if(args[0].toLowerCase() === 'pc-commands') {
-                result = 'You cannot disable that!'
-            } else if(!powercord.pluginManager.isEnabled(args[0])) {
+            if(!powercord.pluginManager.isEnabled(args[0])) {
                 result = `\`${args[0]}\` is already disabled`
             } else {
             result = `\`${args[0]}\` has been disabled`
