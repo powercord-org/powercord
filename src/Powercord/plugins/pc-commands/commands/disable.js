9f5dc33ddff4b7aa4fc6722f4d8eb68b9d180514
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2019-08-11 22:05:58 -0500

    Styling

--- src/Powercord/plugins/pc-commands/commands/disable.js
+++ src/Powercord/plugins/pc-commands/commands/disable.js
@@ -1,44 +1,50 @@
 module.exports = {
-    command: 'disable',
-    description: 'Disable a plugin',
-    usage: '{c} [ PluginID ]',
-    aliases: [],
-    func: (args) => {
-        let result;
-        if(powercord.pluginManager.plugins.has(args[0])) {
-            if(!powercord.pluginManager.isEnabled(args[0])) {
-                result = `\`${args[0]}\` is already disabled`
-            } else {
-            result = `\`${args[0]}\` has been disabled`
-            powercord.pluginManager.disable(args[0])
-        }} else {
-            result = 'You do not have that plugin installed'
-        }
+  command: 'disable',
+  description: 'Allows you to disable and unload a plugin from the given list.',
+  usage: '{c} [pluginID]',
 
-        
-        return { 
-            send: false,
-            result
-        }
-        
-    },
-    autocompleteFunc (args) {
-        const plugins = powercord.pluginManager.getPlugins()
-            .sort((a, b) => a < b ? -1 : 1 || 0)
-            .map(plugin => powercord.pluginManager.plugins.get(plugin));
-        if (args.length > 1) {
-            return false;
-        }
-        return {
-            commands: plugins
-                .filter(plugin => plugin.pluginID !== 'pc-commands' &&
-                    plugin.pluginID.includes(args[0].toLowerCase()))
-                .map(plugin => ({
-                    command: plugin.pluginID,
-                    description: plugin.manifest.description
-                }))
-                .slice(0, 10),
-            header: 'powercord plugin list'
-        };
+  func (args) {
+    let result;
+
+    if (powercord.pluginManager.plugins.has(args[0])) {
+      if (!powercord.pluginManager.isEnabled(args[0])) {
+        result = `->> ERROR: Tried to unload a non-loaded plugin
+            (${args[0]})`;
+      } else {
+        result = `+>> SUCCESS: Plugin unloaded
+              (${args[0]})`;
+        powercord.pluginManager.disable(args[0]);
+      }
+    } else {
+      result = `->> ERROR: Tried to disable a non-installed plugin
+          (${args[0]})`;
     }
-}
+
+    return {
+      send: false,
+      result: `\`\`\`diff\n${result}\`\`\``
+    };
+  },
+
+  autocompleteFunc (args) {
+    const plugins = powercord.pluginManager.getPlugins()
+      .sort((a, b) => a < b ? -1 : 1 || 0)
+      .map(plugin => powercord.pluginManager.plugins.get(plugin));
+
+    if (args.length > 1) {
+      return false;
+    }
+
+    return {
+      commands: plugins
+        .filter(plugin => plugin.pluginID !== 'pc-commands' &&
+          plugin.pluginID.includes(args[0].toLowerCase()))
+        .map(plugin => ({
+          command: plugin.pluginID,
+          description: plugin.manifest.description
+        }))
+        .slice(0, 10),
+      header: 'powercord plugin list'
+    };
+  }
+};
