d6bd6c1cf981bdbd535ee0cc435aae87e3ded0c2
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2019-08-11 22:05:26 -0500

    Styling

--- src/Powercord/plugins/pc-commands/commands/enable.js
+++ src/Powercord/plugins/pc-commands/commands/enable.js
@@ -1,42 +1,49 @@
 module.exports = {
-    command: 'enable',
-    description: 'Enable a plugin',
-    usage: '{c} [ Plugin Name ]',
-    aliases: [],
-    func: (args) => {
-        let result;
-        if(powercord.pluginManager.plugins.has(args[0])) {
-            if(powercord.pluginManager.isEnabled(args[0])) {
-                result = `\`${args[0]}\` is already enabled`
-            } else {
-            result = `\`${args[0]}\` has been enabled`
-            powercord.pluginManager.enable(args[0])
-        }} else {
-            result = 'You do not have that plugin installed'
-        }
+  command: 'enable',
+  description: 'Allows you to (re-)enable and load a plugin from the given list.',
+  usage: '{c} [pluginID]',
 
-        return { 
-            send: false,
-            result
-        }
-    },
-    autocompleteFunc (args) {
-        const plugins = powercord.pluginManager.getPlugins()
-            .sort((a, b) => a < b ? -1 : 1 || 0)
-            .map(plugin => powercord.pluginManager.plugins.get(plugin));
-        if (args.length > 1) {
-            return false;
-        }
-        return {
-            commands: plugins
-                .filter(plugin => plugin.pluginID !== 'pc-commands' &&
-                    plugin.pluginID.includes(args[0].toLowerCase()))
-                .map(plugin => ({
-                    command: plugin.pluginID,
-                    description: plugin.manifest.description
-                }))
-                .slice(0, 10),
-            header: 'powercord plugin list'
-        };
+  func (args) {
+    let result;
+
+    if (powercord.pluginManager.plugins.has(args[0])) {
+      if (powercord.pluginManager.isEnabled(args[0])) {
+        result = `->> ERROR: Tried to load an already loaded plugin
+            (${args[0]})`;
+      } else {
+        result = `+>> SUCCESS: Plugin loaded
+              (${args[0]})`;
+        powercord.pluginManager.enable(args[0]);
+      }
+    } else {
+      result = `->> ERROR: Tried to enable a non-installed plugin
+          (${args[0]})`;
     }
-}
+
+    return {
+      send: false,
+      result: `\`\`\`diff\n${result}\`\`\``
+    };
+  },
+
+  autocompleteFunc (args) {
+    const plugins = powercord.pluginManager.getPlugins()
+      .sort((a, b) => a < b ? -1 : 1 || 0)
+      .map(plugin => powercord.pluginManager.plugins.get(plugin));
+
+    if (args.length > 1) {
+      return false;
+    }
+
+    return {
+      commands: plugins
+        .filter(plugin => plugin.pluginID.includes(args[0].toLowerCase()))
+        .map(plugin => ({
+          command: plugin.pluginID,
+          description: plugin.manifest.description
+        }))
+        .slice(0, 10),
+      header: 'powercord plugin list'
+    };
+  }
+};
