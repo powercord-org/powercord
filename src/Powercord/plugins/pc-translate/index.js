932f700276d4700012018c276a843df144bf3e0e
Harley <GriefMoDz@gmail.com>
2019-08-15 03:28:04 +1000

    [Translate] Improve functionality (closes #147) (#226)
    * [Translate] Improve functionality (closes #147)
    
    * Embed descriptions can now be translated to and from a selected language - support for titles will be available in the near future.
    * Translated messages now keep all original formatting (i.e. Mentions, hyperlinks, etc.) in memory.
    
    * Replace `.pc-` classes and add title translations

--- src/Powercord/plugins/pc-translate/index.js
+++ src/Powercord/plugins/pc-translate/index.js
@@ -1,14 +1,23 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
-const { getModuleByDisplayName, React } = require('powercord/webpack');
-const { sleep, createElement, getOwnerInstance } = require('powercord/util');
+const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { sleep, createElement, forceUpdateElement, getOwnerInstance } = require('powercord/util');
 const { ContextMenu: { Submenu } } = require('powercord/components');
+
 const translate = require('google-translate-api');
 const { resolve } = require('path');
 
 module.exports = class Translate extends Plugin {
   async startPlugin () {
     this.translations = {};
+    this.messageClasses = {
+      ...await getModule([ 'messageCompact', 'messageCozy' ]),
+      ...await getModule([ 'markup' ])
+    };
+
+    Object.keys(this.messageClasses)
+      .forEach(key => this.messageClasses[key] = `.${this.messageClasses[key].replace(/ /g, '.')}`);
+
     this.loadCSS(resolve(__dirname, 'style.scss'));
     this._injectTranslator();
   }
@@ -26,45 +35,140 @@ module.exports = class Translate extends Plugin {
     const _this = this;
     const MessageContent = await getModuleByDisplayName('MessageContent');
     inject('pc-translate-contentRemove', MessageContent.prototype, 'componentWillUnmount', function () {
-      if (_this.translations[this.props.message.id]) {
-        this.props.message.contentParsed = this.original;
-        _this.translations[this.props.message.id] = null;
+      const { message, message: { embeds } } = this.props;
+      const embed = embeds.length > 0 ? embeds[0] : null;
+
+      if (embed && _this.translations[embed.id]) {
+        embed.title = embed.original.title;
+        embed.description = embed.original.description;
+      } else if (_this.translations[message.id]) {
+        message.contentParsed = message.original;
       }
+
+      _this.translations[(embed ? embed : message).id] = null;
     });
 
     inject('pc-translate-content', MessageContent.prototype, 'render', function (args) {
-      if (_this.translations[this.props.message.id]) {
-        this.original = [ ...this.props.message.contentParsed ];
-        this.props.message.contentParsed = [ _this.translations[this.props.message.id] ];
-      } else if (!_this.translations[this.props.message.id] && this.original) {
-        this.props.message.contentParsed = this.original;
-        this.original = null;
+      const { message, message: { embeds } } = this.props;
+      const embed = embeds.length > 0 ? embeds[0] : null;
+
+      if (embed) {
+        if (_this.translations[embed.id] && !embed.original) {
+          // eslint-disable-next-line object-property-newline
+          embed.original = { title: embed.title, description: embed.description };
+          embed.title = _this.translations[embed.id].title;
+          embed.description = _this.translations[embed.id].description;
+        } else if (!_this.translations[embed.id] && embed.original) {
+          embed.title = embed.original.title;
+          embed.description = embed.original.description;
+          embed.original = null;
+        }
       }
+
+      if (_this.translations[message.id] && !message.original) {
+        message.original = [ ...message.contentParsed ];
+
+        if (message.contentParsed.length > 1) {
+          const newContentParsed = message.contentParsed.map((content, index) => {
+            const translations = _this.translations[message.id];
+
+            if (typeof content === 'string') {
+              if (translations.find(translation => content === translation.original)) {
+                const { translation } = translations.find(translation => content === translation.original);
+                message.contentParsed[index] = translation;
+              }
+            }
+
+            return message.contentParsed[index];
+          });
+
+          message.contentParsed = newContentParsed;
+        } else {
+          message.contentParsed = [ _this.translations[message.id] ];
+        }
+      } else if (!_this.translations[message.id] && message.original) {
+        message.contentParsed = message.original;
+        message.original = null;
+      }
+
       return args;
     }, true);
 
     const MessageContextMenu = await getModuleByDisplayName('MessageContextMenu');
-    inject('pc-translate-context', MessageContextMenu.prototype, 'render', function (args, res) {
+    inject('pc-translate-context', MessageContextMenu.prototype, 'render', function (_, res) {
+      const { containerCozyBounded, timestampCozy, markup } = _this.messageClasses;
       const setText = async (opts) => {
-        const cozy = !!this.props.target.closest('.pc-containerCozyBounded');
-        const message = cozy ? this.props.target.closest('.pc-containerCozyBounded') : this.props.target.parentElement.parentElement;
+        const cozy = !!this.props.target.closest(containerCozyBounded);
+        const message = cozy ? this.props.target.closest(containerCozyBounded) : this.props.target.parentElement.parentElement;
 
         message.style.transition = '0.2s';
         message.style.opacity = '0';
 
         let fromLang = '';
 
-        const timestamp = cozy ? message.querySelector('.pc-timestampCozy') : message;
+        const timestamp = cozy ? message.querySelector(timestampCozy) : message;
         await Promise.all([
           sleep(200),
           Promise.all(
-            [ ...message.querySelectorAll('.pc-markup') ]
+            [ ...message.querySelectorAll(markup) ]
               .map(async (markup) => {
                 const markupInstance = getOwnerInstance(markup);
-                const { text, from } = await translate(markupInstance.props.message.contentParsed.filter(c => typeof c === 'string').join(''), opts);
-                _this.translations[markupInstance.props.message.id] = text;
-                fromLang = translate.languages[from.language.iso];
-                markupInstance.forceUpdate();
+                const { embed, message } = markupInstance.props;
+
+                if (embed || (message && message.embeds.length > 0)) {
+                  const embed = markupInstance.props.embed || message.embeds[0];
+
+                  _this.translations[embed.id] = [];
+
+                  if (embed.title) {
+                    const { text } = await translate(embed.title, opts);
+                    _this.translations[embed.id].title = text;
+                  }
+
+                  const { text, from } = await translate(embed.description, opts);
+
+                  _this.translations[embed.id].description = text;
+                  fromLang = translate.languages[from.language.iso];
+                }
+
+                let content;
+
+                if (message) {
+                  if (message.contentParsed.length > 1) {
+                    const contentArray = [];
+                    const contentStrings = message.contentParsed.filter(content => typeof content === 'string');
+
+                    contentStrings.map(async (content) => {
+                      const { text, from } = await translate(content, opts);
+
+                      fromLang = translate.languages[from.language.iso];
+                      contentArray.push({ original: content,
+                        translation: content.startsWith(' ') && content.endsWith(' ')
+                          ? ` ${text} `
+                          : content.startsWith(' ') ? ` ${text}` : content.endsWith(' ') ? `${text} ` : text });
+
+                      return contentArray;
+                    });
+
+                    while (contentArray.length !== contentStrings.length) {
+                      await new Promise(resolve => setTimeout(resolve, 100));
+                    }
+
+                    content = contentArray;
+                  } else {
+                    const { text, from } = await translate(
+                      message.contentParsed.filter(content => typeof content === 'string').join(''),
+                      opts
+                    );
+
+                    fromLang = translate.languages[from.language.iso];
+                    content = text;
+                  }
+
+                  _this.translations[message.id] = content;
+                }
+
+                forceUpdateElement(_this.messageClasses.markup, true);
               })
           )
         ]);
@@ -77,11 +181,21 @@ module.exports = class Translate extends Plugin {
               message.style.opacity = '0';
               await sleep(200);
 
-              message.querySelectorAll('.pc-markup')
+              message.querySelectorAll(markup)
                 .forEach(markup => {
                   const markupInstance = getOwnerInstance(markup);
-                  _this.translations[markupInstance.props.message.id] = null;
-                  markupInstance.forceUpdate();
+                  const { embed, message } = markupInstance.props;
+
+                  if (embed || (message && message.embeds.length > 0)) {
+                    const embed = markupInstance.props.embed || message.embeds[0];
+                    _this.translations[embed.id] = null;
+                  }
+
+                  if (message) {
+                    _this.translations[message.id] = null;
+                  }
+
+                  forceUpdateElement(_this.messageClasses.markup, true);
                 });
 
               timestamp.removeChild(cozy ? this : this.parentElement);
