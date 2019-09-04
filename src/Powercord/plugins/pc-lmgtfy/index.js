e0cc0b409386d4ec55697a2871e26448f78bbcde
Dries1234 <32861260+Dries1234@users.noreply.github.com>
2019-09-04 15:07:03 +0200

    Update index.js

--- src/Powercord/plugins/pc-lmgtfy/index.js
+++ src/Powercord/plugins/pc-lmgtfy/index.js
@@ -1,20 +1,20 @@
 const { Plugin } = require('powercord/entities');
 
 
-const types = ['web','image']
-const sites = ['google','yahoo','bing','ask','aol','duckduckgo']
+const types = [ 'web', 'image' ];
+const sites = [ 'google', 'yahoo', 'bing', 'ask', 'aol', 'duckduckgo' ];
 const siteJson = {
-          'google': 'g',
-          'yahoo':'y',
-          'bing': 'b',
-          'ask': 'k',
-          'aol': 'a',
-          'duckduckgo': 'd'
-        }
+  google: 'g',
+  yahoo:'y',
+  bing: 'b',
+  ask: 'k',
+  aol: 'a',
+  duckduckgo: 'd'
+};
 const typeJson = {
-          'web': 'w',
-          'image': 'i'
-        }
+  web: 'w',
+  image: 'i'
+};
 module.exports = class LMGTFY extends Plugin {
   startPlugin () {
     this.registerCommand(
@@ -23,40 +23,37 @@ module.exports = class LMGTFY extends Plugin {
       'Let me google that for you...',
       '{c} [ ...search terms ] [site] [web|image]',
       (args) => {
-        //set default to google and web search
-        let t = 'w'
-        let s = 'g'
+        // set default to google and web search
+        let t = 'w';
+        let s = 'g';
         // no arguments given
         if (args.length < 1) {
-          return{
+          return {
             send: false,
-            result: 'No Query specified,you need to specify a query.\n Like this for example: \`.lmgtfy A search term google web\`'
-          }
+            result: 'No Query specified,you need to specify a query.\n Like this for example: `.lmgtfy A search term google web`'
+          };
         }
-        let options = args.slice(-2)
-
-      //Loop through both the arrays to check the options specified
+        const options = args.slice(-2);
+        
+        // Loop through both the arrays to check the options specified
         sites.forEach(e => {
-          if(options[0].toLowerCase() === e){
-            s = siteJson[e]
+          if (options[0].toLowerCase() === e) {
+            s = siteJson[e];
         }
       })
 
-      //lmgtfy only supports image for google searches
-      if(s === 'g'){
-        types.forEach(e => {
-          if(options[1].toLowerCase() === e){
-            t = typeJson[e]
-        }
+        // lmgtfy only supports image for google searches
+        if(s === 'g'){
+          types.forEach(e => {
+            if(options[1].toLowerCase() === e){
+              t = typeJson[e];
+          }
       })
-    }
-      
- 
-
-        return{
+      }
+        return {
         send: true,
-        result: `<https://lmgtfy.com/?q=${encodeURI(args.slice(0,args.length-2).join('+'))}&s=${s}&t=${t}>`
-        }
+        result: `<https://lmgtfy.com/?q=${encodeURI(args.slice(0,args.length - 2).join('+'))}&s=${s}&t=${t}>`
+        };
       }
     );
   }
