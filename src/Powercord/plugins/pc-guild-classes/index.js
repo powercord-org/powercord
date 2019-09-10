100cb9a89dc2f146e8263440f1d07bc60efdad22
Cadence Fish <cloudrac3r@vivaldi.net>
2019-09-11 02:10:46 +1200

    how 2 lint

--- src/Powercord/plugins/pc-guild-classes/index.js
+++ src/Powercord/plugins/pc-guild-classes/index.js
@@ -1,57 +1,56 @@
-const {Plugin} = require("powercord/entities")
-const {getOwnerInstance, waitFor} = require("powercord/util")
-const {inject, uninject} = require("powercord/injector")
+const { Plugin } = require('powercord/entities');
+const { getOwnerInstance, waitFor } = require('powercord/util');
+const { inject, uninject } = require('powercord/injector');
 
 class ClassList {
-	constructor(className) {
-		this.tokens = new Set(className.split(" "))
-	}
+  constructor (className) {
+    this.tokens = new Set(className.split(' '));
+  }
 
-	add(c) {
-		this.tokens.add(c)
-	}
+  add (c) {
+    this.tokens.add(c);
+  }
 
-	remove(c) {
-		this.tokens.delete(c)
-	}
+  remove (c) {
+    this.tokens.delete(c);
+  }
 
-	conditional(cond, c) {
-		if (cond) this.add(c)
-		else this.remove(c)
-	}
+  conditional (cond, c) {
+    if (cond) {
+      this.add(c);
+    } else {
+      this.remove(c);
+    }
+  }
 
-	name() {
-		return [...this.tokens.values()].join(" ")
-	}
+  name () {
+    return [ ...this.tokens.values() ].join(' ');
+  }
 }
 
 module.exports = class GuildClasses extends Plugin {
-	constructor() {
-		super()
-	}
+  startPlugin () {
+    this._patchGuild();
+  }
 
-	startPlugin() {
-		this._patchGuild()
-	}
+  pluginWillUnload () {
+    uninject('cadence-guild-classes');
+  }
 
-	pluginWillUnload () {
-		uninject("cadence-guild-classes")
-	}
-
-	async _patchGuild() {
-		const guildInner = await waitFor(".blobContainer-239gwq")
-		const guildElement = guildInner.parentElement
-		const Guild = getOwnerInstance(guildElement)
-		inject("cadence-guild-classes", Guild.constructor.prototype, "render", (_, res) => {
-			if (res._owner) {
-				let hovered = res._owner.memoizedState.hovered
-				let selected = res._owner.pendingProps.selected
-				let classList = new ClassList(res.props.className)
-				classList.conditional(hovered, "pgc-hovered")
-				classList.conditional(selected, "pgc-selected")
-				res.props.className = classList.name()
-			}
-			return res
-		})
-	}
-}
+  async _patchGuild () {
+    const guildInner = await waitFor('.blobContainer-239gwq');
+    const guildElement = guildInner.parentElement;
+    const Guild = getOwnerInstance(guildElement);
+    inject('cadence-guild-classes', Guild.constructor.prototype, 'render', (_, res) => {
+      if (res._owner) {
+        const { hovered } = res._owner.memoizedState;
+        const { selected } = res._owner.pendingProps;
+        const classList = new ClassList(res.props.className);
+        classList.conditional(hovered, 'pgc-hovered');
+        classList.conditional(selected, 'pgc-selected');
+        res.props.className = classList.name();
+      }
+      return res;
+    });
+  }
+};
