8b3bbc0a6b4867df4f66eea3b4d0a9a01302f7b1
Oklar <36753435+LandenStephenss@users.noreply.github.com>
2019-09-28 05:09:01 -0500

    Added disable/enable commands. Also have a plugins (#224)
    * Added disable/enable commands. Also have a plugins
    
    * Added autocomplete
    
    * Added autocomplete
    
    * Small fix
    
    Forgot to remove the pc-commands check
    
    * Update pluginList.js
    
    * Styling
    
    * Styling
    
    * More Styling 
    
    Sorry for all the commits, havnt quite figured out PR's on the command line interface so im using the web version
    
    * Relocate plugin-based commands to 'pc-pluginManager'

---
+++ src/Powercord/plugins/pc-pluginManager/commands/plugins.js
@@ -0,0 +1,20 @@
+module.exports = {
+  command: 'plugins',
+  aliases: [ 'plist' ],
+  description: 'Prints out a list of currently installed plugins.',
+  usage: '{c}',
+
+  func () {
+    const plugins = powercord.pluginManager.getPlugins();
+    const result = {
+      type: 'rich',
+      title: `List of Installed Plugins (${plugins.length})`,
+      description: `\`${plugins.join('\n')}\``
+    };
+
+    return {
+      send: false,
+      result
+    };
+  }
+};
--- src/Powercord/plugins/pc-pluginManager/index.js
+++ src/Powercord/plugins/pc-pluginManager/index.js
@@ -2,10 +2,19 @@ const { Plugin } = require('powercord/entities');
 const { resolve } = require('path');
 
 const Settings = require('./components/Settings.jsx');
+const commands = require('./commands');
 
 module.exports = class PluginManager extends Plugin {
   async startPlugin () {
     this.loadCSS(resolve(__dirname, 'scss', 'style.scss'));
+
+    Object.values(commands).forEach(cmd =>
+      this.registerCommand(cmd.command, cmd.aliases || [],
+        cmd.description, cmd.usage,
+        cmd.func, cmd.autocompleteFunc
+      )
+    );
+
     this.registerSettings('pc-pluginManager', 'Plugins', Settings);
   }
 };
