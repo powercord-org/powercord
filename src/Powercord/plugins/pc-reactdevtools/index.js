9ebf3849767d8f4a10f6934fc240b5b550d5b3b3
Rauf <raufislam2003@gmail.com>
2019-10-14 17:10:42 -0400

    react dev tools without the need for chrome

--- src/Powercord/plugins/pc-reactdevtools/index.js
+++ src/Powercord/plugins/pc-reactdevtools/index.js
@@ -1,60 +1,76 @@
-const { resolve, join } = require('path');
-const { remote } = require('electron');
-const { readdir } = require('fs').promises;
-const { Plugin } = require('powercord/entities');
+const { join } = require("path");
+const { remote } = require("electron");
+const { createWriteStream, existsSync } = require("fs");
+const { Plugin } = require("powercord/entities");
+const request = require("request");
+const unzip = require("unzip-crx");
 
 // stolen from https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/enableReactDevtools.plugin.js
 module.exports = class ReactDevtools extends Plugin {
-  get path () {
-    const path = 'Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi';
-    switch (process.platform) {
-      case 'win32':
-        return resolve(process.env.LOCALAPPDATA, 'Google/Chrome/User Data', path);
-      case 'linux':
-        return resolve(process.env.HOME, '.config/google-chrome', path);
-      case 'darwin':
-        return resolve(process.env.HOME, 'Library/Application Support/Google/Chrome', path);
-    }
-
-    return null;
+  get path() {
+    return join(__dirname, "rdt.crx");
   }
 
-  startPlugin () {
-    /*
-     * We should download our own version of react devtools and use it instead of pulling it from Chrome
-     */
+  get folderPath() {
+    return join(__dirname, "react-dev-tools");
+  }
 
+  get isInstalledLocally() {
+    return existsSync(this.path);
+  }
+
+  download() {
+    return request.get(
+      "https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=77.0.3865.90&acceptformat=crx2&x=id=fmkadmapgofadopljbjfkapdkoienihi%26uc",
+      { encoding: null },
+      (err, res, body) => {
+        if (err) {
+          this.error(err);
+        }
+        if (!Buffer.isBuffer(body)) {
+          this.download();
+        }
+        const file = createWriteStream(this.path);
+        file.write(body, err => {
+          if (err) {
+            this.error(err);
+          }
+          file.close();
+        });
+        unzip(this.path, this.folderPath).then(() => {
+          this.listener();
+          this.log(
+            "If you are unable to find the tabs for React Developer Tools in the Developer Tools, Reload your client (ctrl + r)"
+          );
+        });
+      }
+    );
+  }
+
+  startPlugin() {
     this.listener = this.listener.bind(this);
-    remote.getCurrentWindow().webContents.on('devtools-opened', this.listener);
+    this.download();
+    remote.getCurrentWindow().webContents.on("devtools-opened", this.listener);
     if (remote.getCurrentWindow().webContents.isDevToolsOpened()) {
       this.listener();
     }
   }
 
-  pluginWillUnload () {
-    remote.getCurrentWindow().webContents.removeListener('devtools-opened', this.listener);
+  pluginWillUnload() {
+    remote
+      .getCurrentWindow()
+      .webContents.removeListener("devtools-opened", this.listener);
   }
 
-  async listener () {
-    remote.BrowserWindow.removeDevToolsExtension('React Developer Tools');
+  async listener() {
+    remote.BrowserWindow.removeDevToolsExtension("React Developer Tools");
 
-    let version;
-    let ok = true;
-
-    try {
-      [ version ] = (await readdir(this.path)).slice(-1);
-    } catch (err) {
-      this.log(err);
-      if (err.code !== 'ENOENT') {
-        throw err;
+    if (this.isInstalledLocally) {
+      if (remote.BrowserWindow.addDevToolsExtension(this.folderPath)) {
+        this.log("Successfully installed react devtools.");
+      } else {
+        this.error("Couldn't find react devtools in chrome extensions!");
       }
-      ok = false;
-    }
-
-    if (ok && remote.BrowserWindow.addDevToolsExtension(join(this.path, version))) {
-      this.log('Successfully installed react devtools.');
-    } else {
-      this.error('Couldn\'t find react devtools in chrome extensions!');
     }
   }
 };
