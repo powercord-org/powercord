5ffea91d298b1ac4b8ec9d19afea9dfa0d5d6416
Rauf <raufislam2003@gmail.com>
2019-10-14 18:27:55 -0400

    fix: now using powercord/http, solved linting issues, added a conditional to have it now download on every startup

--- src/Powercord/plugins/pc-reactdevtools/index.js
+++ src/Powercord/plugins/pc-reactdevtools/index.js
@@ -1,75 +1,75 @@
-const { join } = require("path");
-const { remote } = require("electron");
-const { createWriteStream, existsSync } = require("fs");
-const { Plugin } = require("powercord/entities");
-const request = require("request");
-const unzip = require("unzip-crx");
+const { join } = require('path');
+const { remote } = require('electron');
+const { createWriteStream, existsSync } = require('fs');
+const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+const unzip = require('unzip-crx');
 
 // stolen from https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/enableReactDevtools.plugin.js
 module.exports = class ReactDevtools extends Plugin {
-  get path() {
-    return join(__dirname, "rdt.crx");
+  get path () {
+    return join(__dirname, 'rdt.crx');
   }
 
-  get folderPath() {
-    return join(__dirname, "react-dev-tools");
+  get folderPath () {
+    return join(__dirname, 'react-dev-tools');
   }
 
-  get isInstalledLocally() {
+  get isInstalledLocally () {
     return existsSync(this.path);
   }
 
-  download() {
-    return request.get(
-      "https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=77.0.3865.90&acceptformat=crx2&x=id=fmkadmapgofadopljbjfkapdkoienihi%26uc",
-      { encoding: null },
-      (err, res, body) => {
-        if (err) {
-          this.error(err);
-        }
-        if (!Buffer.isBuffer(body)) {
-          this.download();
-        }
-        const file = createWriteStream(this.path);
-        file.write(body, err => {
+  download () {
+    const crxLink = 'https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=77.0.3865.90&acceptformat=crx2&x=id=fmkadmapgofadopljbjfkapdkoienihi%26uc';
+    return get(crxLink).then(res => {
+      this.log(res);
+      return get(res.headers.location).then(resp => {
+        this.log(resp);
+        const crxFile = createWriteStream(this.path);
+        crxFile.write(resp.body, err => {
           if (err) {
             this.error(err);
           }
-          file.close();
+          return crxFile.close();
         });
         unzip(this.path, this.folderPath).then(() => {
           this.listener();
           this.log(
-            "If you are unable to find the tabs for React Developer Tools in the Developer Tools, Reload your client (ctrl + r)"
+            'If you are unable to find the tabs for React Dev Tools in the Developer Tools Tab Bar, reload your client(ctrl + r).'
           );
         });
-      }
-    );
+      }, err => this.error(err));
+    }, err => this.error(err));
   }
 
-  startPlugin() {
+  startPlugin () {
     this.listener = this.listener.bind(this);
-    this.download();
-    remote.getCurrentWindow().webContents.on("devtools-opened", this.listener);
+    if (!this.isInstalledLocally) {
+      this.download();
+    }
+    remote.getCurrentWindow().webContents.on('devtools-opened', this.listener);
     if (remote.getCurrentWindow().webContents.isDevToolsOpened()) {
       this.listener();
     }
   }
 
-  pluginWillUnload() {
+  pluginWillUnload () {
     remote
       .getCurrentWindow()
-      .webContents.removeListener("devtools-opened", this.listener);
+      .webContents.removeListener('devtools-opened', this.listener);
   }
 
-  async listener() {
-    remote.BrowserWindow.removeDevToolsExtension("React Developer Tools");
+  async listener () {
+    remote.BrowserWindow.removeDevToolsExtension('React Developer Tools');
 
     if (this.isInstalledLocally) {
       if (remote.BrowserWindow.addDevToolsExtension(this.folderPath)) {
-        this.log("Successfully installed react devtools.");
+        this.log('Successfully installed react devtools.');
+        this.log(
+          'If React Dev Tools is empty, close Developer Tools and re-open it.'
+        );
       } else {
-        this.error("Couldn't find react devtools in chrome extensions!");
+        this.error('Couldn\'t find react devtools in chrome extensions!');
       }
     }
   }
