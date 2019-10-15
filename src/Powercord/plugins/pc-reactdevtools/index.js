19433b700bb8d8e469ab88ed6470b945b58d5a8a
Rauf Islam <31735267+ItsRauf@users.noreply.github.com>
2019-10-15 15:00:33 -0400

    React Dev Tools without needing it installed in Chrome (#240)
    * react dev tools without the need for chrome
    
    * fix: now using powercord/http, solved linting issues, added a conditional to have it now download on every startup
    
    * add: update checker to make sure you are on the latest version on plugin startup
    
    * fix: didnt see that async there

--- src/Powercord/plugins/pc-reactdevtools/index.js
+++ src/Powercord/plugins/pc-reactdevtools/index.js
@@ -1,30 +1,59 @@
-const { resolve, join } = require('path');
+const { join } = require('path');
 const { remote } = require('electron');
-const { readdir } = require('fs').promises;
+const { createWriteStream, existsSync, readFileSync } = require('fs');
 const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+const unzip = require('unzip-crx');
 
 // stolen from https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/enableReactDevtools.plugin.js
 module.exports = class ReactDevtools extends Plugin {
   get path () {
-    const path = 'Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi';
-    switch (process.platform) {
-      case 'win32':
-        return resolve(process.env.LOCALAPPDATA, 'Google/Chrome/User Data', path);
-      case 'linux':
-        return resolve(process.env.HOME, '.config/google-chrome', path);
-      case 'darwin':
-        return resolve(process.env.HOME, 'Library/Application Support/Google/Chrome', path);
-    }
+    return join(__dirname, 'rdt.crx');
+  }
 
-    return null;
+  get folderPath () {
+    return join(__dirname, 'react-dev-tools');
+  }
+
+  get isInstalledLocally () {
+    return existsSync(this.path);
+  }
+
+  checkForUpdate () {
+    const local = readFileSync(this.path);
+    const crxLink = 'https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=77.0.3865.90&acceptformat=crx2&x=id=fmkadmapgofadopljbjfkapdkoienihi%26uc';
+    return get(crxLink).then(res => {
+      if (res.body !== local) {
+        this.download();
+      }
+    }, err => this.error(err));
+  }
+
+  download () {
+    const crxLink = 'https://clients2.google.com/service/update2/crx?response=redirect&os=win&arch=x86-64&os_arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=unknown&prodversion=77.0.3865.90&acceptformat=crx2&x=id=fmkadmapgofadopljbjfkapdkoienihi%26uc';
+    return get(crxLink).then(res => get(res.headers.location).then(resp => {
+      const crxFile = createWriteStream(this.path);
+      crxFile.write(resp.body, err => {
+        if (err) {
+          this.error(err);
+        }
+        return crxFile.close();
+      });
+      unzip(this.path, this.folderPath).then(() => {
+        this.listener();
+        this.log(
+          'If you are unable to find the tabs for React Dev Tools in the Developer Tools Tab Bar, reload your client(ctrl + r).'
+        );
+      });
+    }, err => this.error(err)), err => this.error(err));
   }
 
   startPlugin () {
-    /*
-     * We should download our own version of react devtools and use it instead of pulling it from Chrome
-     */
-
     this.listener = this.listener.bind(this);
+    if (!this.isInstalledLocally) {
+      this.download();
+    }
+    this.checkForUpdate();
     remote.getCurrentWindow().webContents.on('devtools-opened', this.listener);
     if (remote.getCurrentWindow().webContents.isDevToolsOpened()) {
       this.listener();
@@ -32,29 +61,23 @@ module.exports = class ReactDevtools extends Plugin {
   }
 
   pluginWillUnload () {
-    remote.getCurrentWindow().webContents.removeListener('devtools-opened', this.listener);
+    remote
+      .getCurrentWindow()
+      .webContents.removeListener('devtools-opened', this.listener);
   }
 
-  async listener () {
+  listener () {
     remote.BrowserWindow.removeDevToolsExtension('React Developer Tools');
 
-    let version;
-    let ok = true;
-
-    try {
-      [ version ] = (await readdir(this.path)).slice(-1);
-    } catch (err) {
-      this.log(err);
-      if (err.code !== 'ENOENT') {
-        throw err;
+    if (this.isInstalledLocally) {
+      if (remote.BrowserWindow.addDevToolsExtension(this.folderPath)) {
+        this.log('Successfully installed react devtools.');
+        this.log(
+          'If React Dev Tools is empty, close Developer Tools and re-open it.'
+        );
+      } else {
+        this.error('Couldn\'t find react devtools in chrome extensions!');
       }
-      ok = false;
-    }
-
-    if (ok && remote.BrowserWindow.addDevToolsExtension(join(this.path, version))) {
-      this.log('Successfully installed react devtools.');
-    } else {
-      this.error('Couldn\'t find react devtools in chrome extensions!');
     }
   }
 };
