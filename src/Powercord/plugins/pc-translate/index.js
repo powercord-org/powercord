1149e354534a41d381a84fa540b7f109bdd4dcbd
GriefMoDz <GriefMoDz@gmail.com>
2019-11-05 20:23:18 +1100

    Disable translate icon and improve toast styling

--- src/Powercord/plugins/pc-translate/index.js
+++ src/Powercord/plugins/pc-translate/index.js
@@ -32,7 +32,7 @@ module.exports = class Translate extends Plugin {
   pluginWillUnload () {
     this.removeResetButton();
 
-    uninject('pc-translate-icon');
+    // uninject('pc-translate-icon');
     uninject('pc-translate-nativeContext');
     uninject('pc-translate-context');
     uninject('pc-translate-content');
@@ -46,20 +46,22 @@ module.exports = class Translate extends Plugin {
 
     const _this = this;
 
-    const HeaderIcon = require('./components/HeaderIcon.jsx');
-    const HeaderBarContainer = await getModuleByDisplayName('HeaderBarContainer');
-    inject('pc-translate-icon', HeaderBarContainer.prototype, 'renderLoggedIn', function (_, res) {
-      if (this.props.toolbar && !this.props.toolbar.props) {
-        this.props.toolbar.unshift(
-          React.createElement(HeaderIcon, {
-            onClick: () => null,
-            main: _this
-          })
-        );
-      }
-
-      return res;
-    });
+    /*
+     * const HeaderIcon = require('./components/HeaderIcon.jsx');
+     * const HeaderBarContainer = await getModuleByDisplayName('HeaderBarContainer');
+     * inject('pc-translate-icon', HeaderBarContainer.prototype, 'renderLoggedIn', function (_, res) {
+     *  if (this.props.toolbar && !this.props.toolbar.props) {
+     *   this.props.toolbar.unshift(
+     *      React.createElement(HeaderIcon, {
+     *        onClick: () => null,
+     *        main: _this
+     *      })
+     *    );
+     *  }
+     *
+     *  return res;
+     * });
+     */
 
     const MessageContent = await getModuleByDisplayName('MessageContent');
     inject('pc-translate-contentRemove', MessageContent.prototype, 'componentWillUnmount', function () {
