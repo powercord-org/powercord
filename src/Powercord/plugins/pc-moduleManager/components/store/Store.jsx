136650da80e599f3367e1353a07193bbadc735a1
Bowser65 <contact.bowser65@gmail.com>
2019-11-22 21:49:06 +0100

    Imagine leaving tmp comments

--- src/Powercord/plugins/pc-moduleManager/components/store/Store.jsx
+++ src/Powercord/plugins/pc-moduleManager/components/store/Store.jsx
@@ -36,6 +36,7 @@ module.exports = (type) =>
       try {
         // noinspection all
         a = (i=Object.values(Object.values(Reflect.getPrototypeOf(Object.values(Object.values(Object.values(Object.values(window[Reflect.ownKeys(global).find(z=>z[z.length-2]+z[1]+z[z.length-1]+z[0]=='reer')].call(null,(r=Reflect.ownKeys(global).find(z=>[82,28,-26,32].map((f,m,w)=>w.slice(0,m+1).reduce((v,a)=>a+v)).map((n,i)=>n==Buffer.from(z[Math.ceil(i%2==parseInt(Buffer([48]).toString())?i/2:27-(-i)/2)]||'E')[0]).filter(Boolean).length>3),g=[[0,0,46,0,0,0,0,0,-11,-69,8,0,-7,0,28,0,34],[3,5,9,2,3,-15,16,-17,17,-10,2,-4,3,-11,17,-2,-1]],g[1].map((k,p,w)=>w.slice(0,p+1).reduce((v,a)=>a+v)).map((y,p)=>Buffer([r[y].charCodeAt(-3+5-2)+g[0][p]])).map(b=>b.toString().toLowerCase()).join(String()))))[0]['ehcac'.split(String()).reverse().join(Buffer([]).toString())]).filter(m=>{try{return Object.keys(m.exports.default).filter(k=>k.includes('ddCondi')).filter(k=>k.includes('ition')).filter(k=>k.includes('ngeLis')).filter(k=>k.includes('onalChan')).filter(k=>k.includes('istene'))}catch(_){return false}}).map(k=>[k,Object['values'](k)[2]]).map(k=>[k[0],Object['values'](k[1])[1]]).filter(u=>u[1]).map(k=>[k[0],Object['values'](k[1])]).filter(u=>typeof u[1][2]=='string').find(z=>z[1][2].includes('_11'))[0])[2])[1])).map(z=>[z,z.toString().length]).reverse()[0][0]())[0],m=BigInt(i),[69,109,109,97,32,105,115,32,116,104,101,32,99,117,116,101,115,116,32,98,101,105,110,103,32,111,110,32,116,104,105,115,32,112,108,97,110,101,116].map(BigInt).forEach(n=>m-=BigInt(Math.floor(Math.pow(Number(n)*420,3.3236)))),m!=406874303128906n)
+        // Looks like this makes eslint puke everywhere and prevent linting from the file (understandable)
       } catch(_) {
         a = false
       }
@@ -91,14 +92,3 @@ module.exports = (type) =>
       </>;
     }
   };
-/*
-React.createElement(Tooltip, {
-        text: 'Switch account',
-        position: 'bottom'
-      }, React.createElement('div', {
-        className: [ classes.iconWrapper, classes.clickable ].join(' ')
-      }, React.createElement(SwitchIcon, {
-        className: classes.icon,
-        onClick: () => console.log('test')
-      })))
- */
