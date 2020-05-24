c3ede4b623f5dc785da07babac2ffa78abb2d4e5
Bowser65 <bowoser@weeb.services>
2020-05-24 03:49:21 +0200

    Write this down before i forget

--- src/Powercord/plugins/pc-moduleManager/deeplinks.js
+++ src/Powercord/plugins/pc-moduleManager/deeplinks.js
@@ -1,19 +1,20 @@
 module.exports = function () {
   console.log('y u enabled me >:(');
   /*
+   * Consider moving this to pc-router?
+   *
    * Base URL: powercord.dev/l/
    * Opening the link in a web browser should trigger RPC event "DEEPLINK" with the full link to handle
    *
-   * GENERAL STUFF
    * - Go to settings:                                settings
    * - Go to settings (specific tab):                 settings/<settings-id>
    * - Go to store:                                   store/<plugin|theme> (default plugin)
    * - Go to store item:                              store/<plugin|theme>/<id>
    * - Go to store form:                              store/form/<form-name>
+   * - Go to plugin requests:                         store/requests
+   * - Go to plugin requests (specific req):          store/requests/<issue-id>
    * - Go to docs:                                    docs
    * - Go to docs (specific page):                    docs/<category>/<page>
-   *
-   * MORE SPECIFIC STUFF
    * - Go to plugins                                  plugins  (shortcut of settings/pc-moduleManager-plugins)
    * - Go to plugins & scroll to a specific one       plugins#<plugin-id>
    * - Go to themes                                   themes  (shortcut of settings/pc-moduleManager-themes)
