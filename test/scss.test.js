6d6b4fd34ebbd59571f3e3eee1ca09ba17a78881
Bowser65 <bowoser@weeb.services>
2020-05-31 20:32:16 +0200

    Change Powercord's license
    The GPL doesn't allow writing plugins for proprietary software.
    Since we're a plugin for Discord, the GPL wasn't applicable.

--- test/scss.test.js
+++ test/scss.test.js
@@ -1,22 +1,4 @@
 /* eslint-env jest */
-/**
- * Powercord, a lightweight @discord client mod focused on simplicity and performance
- * Copyright (C) 2018-2020  aetheryx & Bowser65
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 const { join } = require('path');
 const ScssCompiler = require('../src/fake_node_modules/powercord/compilers/scss');
 const rmdir = require('../src/fake_node_modules/powercord/util/rmdirRf');
