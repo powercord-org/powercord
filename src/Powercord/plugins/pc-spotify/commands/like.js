0a0fadb4a379f9df212dfeac0a4fbdb7ba5bb7a0
StavZ <randy10220@gmail.com>
2020-06-28 12:34:27 +0300

    like command | Allows you add current playing song to your liked songs by command

---
+++ src/Powercord/plugins/pc-spotify/commands/like.js
@@ -0,0 +1,29 @@
+const playerStore = require('../playerStore/store');
+const SpotifyAPI = require('../SpotifyAPI');
+
+module.exports = {
+  command: 'like',
+  description: 'Like a current playing song',
+  async executor () {
+    const currentTrack = playerStore.getCurrentTrack();
+    if (!currentTrack) {
+      return {
+        send: false,
+        result: 'You are not currently listening to anything.'
+      };
+    }
+    const { body } = await SpotifyAPI.checkLibrary(currentTrack.id);
+    if (body[0]) {
+      SpotifyAPI.removeSong(currentTrack.id);
+      return {
+        send: false,
+        result: `You removed **${currentTrack.name} by ${currentTrack.artists}** from your Liked Songs`
+      };
+    }
+    SpotifyAPI.addSong(currentTrack.id);
+    return {
+      send: false,
+      result: `You added **${currentTrack.name} by ${currentTrack.artists}** to your Liked Songs`
+    };
+  }
+};
