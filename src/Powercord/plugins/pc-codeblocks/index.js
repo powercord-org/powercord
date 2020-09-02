91db4d4e8b2ec02e7f08bba67c84a03cfb7cca3c
Bowser65 <bowoser@weeb.services>
2020-09-02 18:34:16 +0200

    Fix codeblock line numbers

--- src/Powercord/plugins/pc-codeblocks/index.js
+++ src/Powercord/plugins/pc-codeblocks/index.js
@@ -1,20 +1,19 @@
 const { Plugin } = require('powercord/entities');
-const { React, getModule, hljs } = require('powercord/webpack');
+const { React, getModule, hljs, i18n: { Messages } } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
-const { findInReactTree } = require('powercord/util');
+const { getReactInstance, findInReactTree } = require('powercord/util');
 const { clipboard } = require('electron');
 
 module.exports = class Codeblocks extends Plugin {
   startPlugin () {
-    this.loadStylesheet('style.scss');
+    this.loadStylesheet('style.css');
     this.patchCodeblocks();
   }
 
   pluginWillUnload () {
     uninject('pc-codeblocks-inline');
     uninject('pc-codeblocks-embed');
-
-    document.querySelectorAll('.hljs [class^=powercord]').forEach(e => e.style.display = 'none');
+    this._forceUpdate();
   }
 
   async patchCodeblocks () {
@@ -35,6 +34,8 @@ module.exports = class Codeblocks extends Plugin {
 
       return res;
     });
+
+    this._forceUpdate();
   }
 
   injectCodeblock (args, codeblock) {
@@ -45,68 +46,57 @@ module.exports = class Codeblocks extends Plugin {
 
       const { children } = res.props;
       const lang = args ? args[0].lang : children.props.className.split(' ').find(className => !className.includes('-') && className !== 'hljs');
+      const isHljs = !!children.props.dangerouslySetInnerHTML;
+      const lines = children.props.dangerouslySetInnerHTML
+        ? children.props.dangerouslySetInnerHTML.__html
+          // Ensure there is no span on multiple lines
+          .replace(/<span class="(hljs-[a-z]+)">([^<]*)\n/g, '<span class="$1">$2</span>\n<span class="$1">')
+          .split('\n')
+        : children.props.children.split('\n');
 
-      if (children.props.dangerouslySetInnerHTML) {
-        children.props.children = this.renderCodeblock(lang, children.props.dangerouslySetInnerHTML);
-
-        delete children.props.dangerouslySetInnerHTML;
-      } else if (typeof children.props.children === 'string') {
-        children.props.children = this.renderCodeblock(lang, children.props.children);
-      }
-
+      delete children.props.dangerouslySetInnerHTML;
+      children.props.children = this.renderCodeblock(lang, lines, isHljs);
       return res;
     };
   }
 
-  renderCodeblock (lang, content) {
-    const children = [];
-    const isDangerouslySetInnerHTML = typeof content === 'object';
-    const isValidLanguage = typeof hljs.getLanguage(lang) !== 'undefined';
+  renderCodeblock (lang, lines, isHljs) {
+    const language = hljs.getLanguage(lang);
 
-    children.push(React.createElement('div', {
-      dangerouslySetInnerHTML: isDangerouslySetInnerHTML ? content : null
-    }, isDangerouslySetInnerHTML ? null : content), isValidLanguage && React.createElement('div', {
-      className: 'powercord-codeblock-lang'
-    }, lang), React.createElement('div', {
-      className: 'powercord-lines'
-    }), React.createElement('button', {
-      className: 'powercord-codeblock-copy-btn',
-      onClick: this._onClickHandler
-    }, 'copy'));
-
-    return children;
+    return React.createElement(React.Fragment, null,
+      language && React.createElement('div', { className: 'powercord-codeblock-lang' }, language.name),
+      React.createElement('table', { className: 'powercord-codeblock-table' },
+        ...lines.map((line, i) => React.createElement('tr', null,
+          React.createElement('td', null, i + 1),
+          React.createElement('td', isHljs ? { dangerouslySetInnerHTML: { __html: line } } : { children: line })
+        ))
+      ),
+      React.createElement('button', {
+        className: 'powercord-codeblock-copy-btn',
+        onClick: this._onClickHandler
+      }, Messages.COPY)
+    );
   }
 
   _onClickHandler (e) {
     const { target } = e;
-
     if (target.classList.contains('copied')) {
       return;
     }
 
-    target.innerText = 'copied!';
+    target.innerText = Messages.ACCOUNT_USERNAME_COPY_SUCCESS_1;
     target.classList.add('copied');
 
     setTimeout(() => {
-      target.innerText = 'copy';
+      target.innerText = Messages.COPY;
       target.classList.remove('copied');
     }, 1000);
 
-    const codeContent = target.parentElement.children[0];
-    const pcCopy = codeContent.querySelector('[data-powercord-codeblock-copy]');
-    if (pcCopy) {
-      return clipboard.writeText(pcCopy.textContent);
-    }
+    const code = [ ...target.parentElement.querySelectorAll('td:last-child') ].map(t => t.textContent).join('\n');
+    clipboard.writeText(code);
+  }
 
-    const range = document.createRange();
-    range.selectNode(codeContent);
-
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    clipboard.writeText(selection.toString());
-
-    selection.removeAllRanges();
+  _forceUpdate () {
+    document.querySelectorAll('[id^="chat-messages-"]').forEach(e => getReactInstance(e).memoizedProps.onMouseMove());
   }
 };
