5a6e1ccff284fccba2590d83996694d288eff1e8
redstonekasi <redstonekasi@gmail.com>
2020-09-21 19:47:06 +0200

    fix announcements

--- src/Powercord/plugins/pc-notices/components/Announcement.jsx
+++ src/Powercord/plugins/pc-notices/components/Announcement.jsx
@@ -16,23 +16,22 @@ class Announcement extends React.PureComponent {
 
   async componentDidMount () {
     if (!classesCache) {
-      const classes = await getModule([ 'noticeBrand' ]);
+      const classes = await getModule([ 'colorPremiumTier1' ]);
       classesCache = {
         types: {
-          blurple: classes.noticeBrand,
-          red: classes.noticeDanger,
-          orange: classes.noticeDefault,
-          facebook: classes.noticeFacebook,
-          blue: classes.noticeInfo,
-          dark: classes.noticePremium,
-          blurple_gradient: classes.noticePremiumGrandfathered,
-          spotify: classes.noticeSpotify,
-          purple: classes.noticeStreamerMode,
-          green: classes.noticeSuccess,
-          survey: classes.noticeSurvey
+          blurple: classes.colorBrand,
+          red: classes.colorDanger,
+          orange: classes.colorDefault,
+          blue: classes.colorInfo,
+          dark: classes.colorDark,
+          blurple_gradient: classes.colorPremiumTier1,
+          spotify: classes.colorSpotify,
+          purple: classes.colorStreamerMode,
+          green: classes.colorSuccess
         },
         button: classes.button,
-        dismiss: classes.dismiss
+        dismiss: classes.closeButton,
+        notice: classes.notice
       };
 
       this.setState(classesCache);
@@ -40,9 +39,9 @@ class Announcement extends React.PureComponent {
   }
 
   render () {
-    const { types, button, dismiss } = this.state;
+    const { types, button, dismiss, notice } = this.state;
 
-    return <div className={`powercord-notice ${(types[this.props.color] || types.blurple)}`} id={this.props.id}>
+    return <div className={`powercord-notice ${notice} ${(types[this.props.color] || types.blurple)}`} id={this.props.id}>
       {this.props.message}
       <Clickable className={dismiss} onClick={() => this.handleClick(this.props.onClose)}/>
       {this.props.button && <button className={button} onClick={() => this.handleClick(this.props.button.onClick)}>
