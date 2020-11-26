9ac9deec1181ebaef5b22cefba87261f5416d70a
Dutake <74233503+dutake@users.noreply.github.com>
2020-11-25 16:33:28 -0800

    formatting

--- src/Powercord/plugins/pc-tags/commands/list.js
+++ src/Powercord/plugins/pc-tags/commands/list.js
@@ -3,12 +3,11 @@ module.exports = {
   description: 'Get a list of all tags you have',
   executor: (_, { settings }) => {
     const keys = settings.getKeys();
-
     return {
       send: false,
       result: {
         type: 'rich',
-        title: `You have ${keys.length} ${keys.length == 1 ? 'tag' : 'tags'} available:`,
+        title: `You have ${keys.length} ${keys.length === 1 ? 'tag' : 'tags'} available:`,
         description: keys.map(key => `- ${key}`).join('\n')
       }
     };
