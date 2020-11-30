cd5bd35be7a7a9bc48edb7cb1bdb71e2b67795c3
WickedTree Development <johannovak7@gmail.com>
2020-11-30 10:07:49 -0800

    Use path.join instead of concatenation

--- injectors/darwin.js
+++ injectors/darwin.js
@@ -3,10 +3,11 @@
  * All Rights Reserved. Licensed under the Porkord License
  * https://powercord.dev/porkord-license
  */
+const path = require('path');
 if (process.argv.includes('--install-path')) {
   installPath = process.argv[process.argv.indexOf('--install-path') + 1]
   console.log('\x1b[35m%s\x1b[0m', 'Installing to ' + installPath)
-  exports.getAppDir = async () => installPath + '/Contents/Resources/app'
+  exports.getAppDir = async () => path.join(installPath, 'Contents/Resources/app')
 } else {
   exports.getAppDir = async () => '/Applications/Discord Canary.app/Contents/Resources/app';
 }
