825f5d2412abb61a2c03323f81f9a93bee9b67f0
Cynthia <cynthia@cynthia.dev>
2021-09-12 13:17:18 +0200

    do not track stuff

--- src/Powercord/coremods/no-track/index.js
+++ src/Powercord/coremods/no-track/index.js
@@ -4,45 +4,44 @@
  * https://powercord.dev/porkord-license
  */
 
-// todo: rewrite using webFrame evaluation?
-const { getModule } = require('powercord/webpack');
+const { webFrame } = require('electron');
+
+async function inject () {
+  window.__$$DoNotTrackCache = {};
+  const Analytics = await window.$PowercordWebpack.getModule([ 'getSuperPropertiesBase64' ]);
+  const Reporter = await window.$PowercordWebpack.getModule([ 'submitLiveCrashReport' ]);
+  const AnalyticsMaker = await window.$PowercordWebpack.getModule([ 'analyticsTrackingStoreMaker' ]);
+
+  window.__$$DoNotTrackCache.oldTrack = Analytics.track;
+  window.__$$DoNotTrackCache.oldSubmitLiveCrashReport = Reporter.submitLiveCrashReport;
+  window.__$$DoNotTrackCache.oldAddBreadcrumb = window.__SENTRY__.hub.addBreadcrumb;
+  window.__$$DoNotTrackCache.oldHandleTrack = AnalyticsMaker.AnalyticsActionHandlers.handleTrack;
+
+  Analytics.track = () => void 0;
+  Reporter.submitLiveCrashReport = () => void 0;
+  AnalyticsMaker.AnalyticsActionHandlers.handleTrack = () => void 0;
+  window.__SENTRY__.hub.addBreadcrumb = () => void 0;
+
+  window.__SENTRY__.hub.getClient().close();
+  window.__SENTRY__.hub.getScope().clear();
+}
+
+function uninject () {
+  const Analytics = window.$PowercordWebpack.getModule([ 'getSuperPropertiesBase64' ], false);
+  const Reporter = window.$PowercordWebpack.getModule([ 'submitLiveCrashReport' ], false);
+  const AnalyticsMaker = window.$PowercordWebpack.getModule([ 'analyticsTrackingStoreMaker' ], false);
+
+  Analytics.track = window.__$$DoNotTrackCache.oldTrack;
+  Reporter.submitLiveCrashReport = window.__$$DoNotTrackCache.oldSubmitLiveCrashReport;
+  window.__SENTRY__.hub.addBreadcrumb = window.__$$DoNotTrackCache.oldAddBreadcrumb;
+  AnalyticsMaker.AnalyticsActionHandlers.handleTrack = window.__$$DoNotTrackCache.oldHandleTrack;
+  delete window.__$$DoNotTrackCache;
+}
 
 module.exports = async function () {
-  const Analytics = await getModule([ 'getSuperPropertiesBase64' ]);
-  const Reporter = await getModule([ 'submitLiveCrashReport' ]);
-  const Sentry = {
-    main: window.__SENTRY__.hub,
-    client: window.__SENTRY__.hub.getClient()
-  };
-
-  Analytics.__oldTrack = Analytics.track;
-  Analytics.track = () => void 0;
-
-  Reporter.__oldSubmitLiveCrashReport = Reporter.submitLiveCrashReport;
-  Reporter.submitLiveCrashReport = () => void 0;
-
-  Sentry.client.close();
-  Sentry.main.getScope().clear();
-  Sentry.main.__oldAddBreadcrumb = Sentry.main.addBreadcrumb;
-  Sentry.main.addBreadcrumb = () => void 0;
-
-  window.__oldConsole = window.console;
-  Object.assign(window.console, [ 'debug', 'info', 'warn', 'error', 'log', 'assert' ].forEach(
-    (method) => {
-      // @todo: figure out react devtools and if this is necessary
-      if (window.console[method].__sentry_original__) {
-        window.console[method] = window.console[method].__sentry_original__;
-      } else if (window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__) {
-        window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__ = window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__.__sentry_original__;
-      }
-    })
-  );
+  webFrame.executeJavaScript(`(() => { ${inject.toString()} inject(); })();`);
 
   return () => {
-    Analytics.track = Analytics.__oldTrack;
-    Reporter.submitLiveCrashReport = Reporter.__oldSubmitLiveCrashReport;
-    Sentry.main.addBreadcrumb = Sentry.main.__oldAddBreadcrumb;
-    Sentry.client.getOptions().enabled = true;
-    window.console = window.__oldConsole;
+    webFrame.executeJavaScript(`(() => { ${uninject.toString()} uninject(); })();`);
   };
 };
