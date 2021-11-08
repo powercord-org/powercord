41160621c8b93386f002f4c2817ce059b9bc7a17
Krisztián Hozák <pistahozak@gmail.com>
2021-11-08 17:13:42 +0100

    Added .DS_Store fix, see bug #571

--- src/Powercord/modules/index.js
+++ src/Powercord/modules/index.js
@@ -1,4 +1,5 @@
 module.exports = require('fs')
   .readdirSync(__dirname)
   .filter(file => file !== 'index.js')
+  .filter(file => file !== '.DS_Store')
   .map(filename => require(`${__dirname}/${filename}`));
