4490f6d638674d0bd40e139ee3f564257389a4b8
Katlyn Lorimer <17418611+katlyn@users.noreply.github.com>
2021-11-26 13:07:06 -0900

    fix: filter should be inverted

--- src/Powercord/modules/index.js
+++ src/Powercord/modules/index.js
@@ -1,4 +1,4 @@
 module.exports = require('fs')
   .readdirSync(__dirname)
-  .filter(file => ['index.js', '.DS_Store'].includes(file))
+  .filter(file => !['index.js', '.DS_Store'].includes(file))
   .map(filename => require(`${__dirname}/${filename}`));
