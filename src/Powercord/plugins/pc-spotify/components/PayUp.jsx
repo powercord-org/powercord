00a2127efeb66c1ffd7bea8f5b2b2dad27d88c8b
cafeed28 <artlekseev@gmail.com>
2021-12-23 23:03:27 +0500

    pc-spotify changes: almost full i18n support, changed padding of duration and changed advanced controls settings

--- src/Powercord/plugins/pc-spotify/components/PayUp.jsx
+++ src/Powercord/plugins/pc-spotify/components/PayUp.jsx
@@ -11,26 +11,18 @@ module.exports = React.memo(
     return (
       <Modal className='powercord-text'>
         <Modal.Header>
-          <FormTitle tag='h4'>Spotify Premium Required</FormTitle>
+          <FormTitle tag='h4'>{Messages.SPOTIFY_PAYUP_TITLE}</FormTitle>
           <Modal.CloseButton onClick={() => closeModal()}/>
         </Modal.Header>
         <Modal.Content>
           <div className={`${size16} ${marginBottom20}`}>
-            To control your Spotify playback we use Spotify's "Connect Web" API, which is unfortunately locked to
-            Spotify Premium users. In order for you to control Spotify's playback you'll need to get a Premium
-            subscription.
+            {Messages.SPOTIFY_PAYUP_TEXT_1}
           </div>
           <div className={`${size16} ${marginBottom20}`}>
-            If you do happen to have a Spotify Premium subscription but you're still not seeing the buttons show up,
-            it might happen that Spotify is reporting inaccurate data about your Premium status and alters the
-            availability of the buttons. Try changing the playback in any way (play, pause, change track, ...) to
-            trigger and update and let us get accurate data from Spotify.
+            {Messages.SPOTIFY_PAYUP_TEXT_2}
           </div>
           <div className={`${size16} ${marginBottom20}`}>
-            If this still did not fix it, make sure you're not in a private session as we've received a few reports
-            saying this causes your Premium subscription status to not be properly sent to us. Also make sure you do
-            have your Spotify account linked to your Discord account (You can tell Discord to not show it on your
-            profile and not show the song you're listening to in your status, if you don't want to)
+            {Messages.SPOTIFY_PAYUP_TEXT_3}
           </div>
         </Modal.Content>
         <Modal.Footer>
