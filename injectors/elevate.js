5e3c208b5ec668d76d13ab953161e3216d027cbf
Vendicated <vendicated@riseup.net>
2022-04-27 14:13:10 +0200

    Injector(linux): Add doas & plain su support

--- injectors/elevate.js
+++ injectors/elevate.js
@@ -1,8 +1,22 @@
 const { spawnSync } = require('child_process');
 
-// It seems `sudo npm ...` no longer give the script sudo perms in npm v7, so here we are.
+// It seems `sudo npm ...` no longer gives the script sudo perms in npm v7, so here we are.
 if (process.platform === 'linux' && process.getuid() !== 0) {
-  // todo: decide whether this is a viable solution for the long term. people may use doas or other tools for elevating themselves.
-  spawnSync('sudo', process.argv, { stdio: 'inherit' })
+  tryToElevate('sudo')
+  tryToElevate('doas')
+
+  console.warn('Neither doas nor sudo were found. Falling back to su.')
+  console.log('Please enter your root password')
+  spawnSync('su', ['-c', process.argv.join(' ')], { stdio: 'inherit'})
   process.exit(0)
+}
+
+function tryToElevate(command) {
+  const { error } = spawnSync(command, process.argv, { stdio: 'inherit'})
+  if (!error) { 
+    process.exit(0)
+  } else if (error.code !== 'ENOENT') {
+    console.error(error)
+    process.exit(1)
+  }
 }
