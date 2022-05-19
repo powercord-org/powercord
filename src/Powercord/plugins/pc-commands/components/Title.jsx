81c66abd6b3ad67944f380ab50ffab88e2c55f77
Katlyn Lorimer <katlyn@katlyn.dev>
2022-05-19 11:33:19 -0800

    fix: don't crash on command autocomplete
    This commit fixes #643 by using the vanilla Autocomplete.Title
    component. After testing, there is no regression that I could find from
    using the unpatched component rather than custom patched version.

--- src/Powercord/plugins/pc-commands/components/Title.jsx
+++ src/Powercord/plugins/pc-commands/components/Title.jsx
@@ -2,14 +2,4 @@ const { React, getModuleByDisplayName } = require('powercord/webpack');
 
 const Autocomplete = getModuleByDisplayName('Autocomplete', false);
 
-module.exports = class Title extends Autocomplete.Title {
-  render () {
-    const res = super.render();
-
-    if (!this.props.title[0]) {
-      return <div style={{ padding: '4px' }}/>;
-    }
-
-    return res;
-  }
-};
+module.exports = Autocomplete.Title;
