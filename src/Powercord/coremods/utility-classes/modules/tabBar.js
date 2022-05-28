2f6687e833e21e44198e5a9e35422c034402bbf5
GriefMoDz <griefmodz@gmail.com>
2022-05-28 13:25:47 +1000

    fix: tab bar error spam
    closes #607

--- src/Powercord/coremods/utility-classes/modules/tabBar.js
+++ src/Powercord/coremods/utility-classes/modules/tabBar.js
@@ -4,7 +4,7 @@ const { inject, uninject } = require('powercord/injector');
 module.exports = async () => {
   const TabBar = await getModuleByDisplayName('TabBar');
   inject('pc-utilitycls-tabbar', TabBar.Item.prototype, 'render', function (_, res) {
-    if (this.props.id) {
+    if (typeof this.props.id === 'string') {
       res.props['data-item-id'] = this.props.id.replace(/&/g, 'n').replace(/ /g, '');
     }
     return res;
