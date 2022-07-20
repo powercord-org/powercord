74f36771d5db9617519b5190b210e47089997764
aetheryx <zainalih3c@gmail.com>
2022-07-20 15:58:28 +0200

    filter plugin list

--- src/Powercord/plugins/pc-moduleManager/commands/plugins.js
+++ src/Powercord/plugins/pc-moduleManager/commands/plugins.js
@@ -1,10 +1,14 @@
+const { CORE_PLUGINS } = require('powercord/constants');
+
 module.exports = {
   command: 'plugins',
   aliases: [ 'plist' ],
   description: 'Prints out a list of currently installed plugins.',
   usage: '{c}',
   executor () {
-    const plugins = powercord.pluginManager.getPlugins();
+    const plugins = powercord.pluginManager.getPlugins()
+      .filter(p => !CORE_PLUGINS.includes(p));
+
     const result = {
       type: 'rich',
       title: `List of Installed Plugins (${plugins.length})`,
