3677b93d95ff739b58de760fbcc5c305f5008c13
aetheryx <zainalih3c@gmail.com>
2022-07-20 15:38:38 +0200

    refactor

--- src/Powercord/plugins/pc-moduleManager/commands/disable.js
+++ src/Powercord/plugins/pc-moduleManager/commands/disable.js
@@ -1,63 +1,71 @@
 const { CORE_PLUGINS } = require('powercord/constants');
 
+const resp = (success, title) => ({
+  send: false,
+  result: {
+    type: 'rich',
+    color: success ? 0x1bbb1b : 0xdd2d2d,
+    title: `${success ? 'SUCCESS:' : 'ERROR:'} ${title}`,
+  },
+});
+
 module.exports = {
   command: 'disable',
   description: 'Disable a plugin/theme',
   usage: '{c} [ plugin/theme ID ]',
-  executor (args) {
-    let result;
-
-    const isPlugin = powercord.pluginManager.plugins.has(args[0]);
-    const isTheme = powercord.styleManager.themes.has(args[0]);
+  resp,
+  executor([ id ]) {
+    const isPlugin = powercord.pluginManager.plugins.has(id);
+    const isTheme = powercord.styleManager.themes.has(id);
 
     if (!isPlugin && !isTheme) { // No match
-      result = `->> ERROR: Could not find plugin or theme matching that name.
-      (${args[0]})`;
+      return resp(false, `Could not find plugin or theme matching "${id}".`)
     } else if (isPlugin && isTheme) { // Duplicate name
-      result = `->> ERROR: This name is in use by both a plugin and theme. You will have to disable it from settings.
-      (${args[0]})`;
-    } else if (isPlugin && CORE_PLUGINS.includes(args[0])) { // Core internal plugin
-      result = `->> ERROR: This plugin provides core functionality and cannot be disabled.
-      (${args[0]})`;
-    } else {
-      const manager = isPlugin ? powercord.pluginManager : powercord.styleManager;
-      if (!manager.isEnabled(args[0])) {
-        result = `->> ERROR: Tried to disable an already disabled ${isPlugin ? 'plugin' : 'theme'}!
-        (${args[0]})`;
-      } else {
-        manager.disable(args[0]);
-        result = `+>> SUCCESS: ${isPlugin ? 'Plugin' : 'Theme'} disabled!
-        (${args[0]})`;
-      }
+      return resp(false, `"${id}" is in use by both a plugin and theme. You will have to disable it from settings.`);
+    } else if (isPlugin && CORE_PLUGINS.includes(id)) { // Core internal plugin
+      return resp(false, `"${id}" provides core functionality and cannot be disabled.`);
     }
 
-    return {
-      send: false,
-      result: `\`\`\`diff\n${result}\`\`\``
-    };
+    const manager = isPlugin ? powercord.pluginManager : powercord.styleManager;
+    if (!manager.isEnabled(id)) {
+      return resp(false, `"${id}" is already disabled.`)
+    }
+
+    manager.disable(id);
+    return resp(true, `${isPlugin ? 'Plugin' : 'Theme'} "${id}" disabled!`);
   },
 
-  autocomplete (args) {
+  autocomplete(args) {
     const plugins = Array.from(powercord.pluginManager.plugins.values())
-      .filter(plugin => !CORE_PLUGINS.includes(plugin) && plugin.entityID !== 'pc-commands' &&
-        plugin.entityID.toLowerCase().includes(args[0] && args[0].toLowerCase()) && powercord.pluginManager.isEnabled(plugin.entityID));
+      .filter(plugin =>
+        !CORE_PLUGINS.includes(plugin) &&
+        plugin.entityID !== 'pc-commands' &&
+        plugin.entityID.toLowerCase().includes(args[0]?.toLowerCase()) &&
+        powercord.pluginManager.isEnabled(plugin.entityID)
+      );
 
     const themes = Array.from(powercord.styleManager.themes.values())
-      .filter(theme => theme.entityID.toLowerCase().includes(args[0] && args[0].toLowerCase()) && powercord.styleManager.isEnabled(theme.entityID));
+      .filter(theme =>
+        theme.entityID.toLowerCase().includes(args[0]?.toLowerCase()) &&
+        powercord.styleManager.isEnabled(theme.entityID)
+      );
 
     if (args.length > 1) {
       return false;
     }
 
     return {
-      commands: [ ...plugins.map(plugin => ({
-        command: plugin.entityID,
-        description: `Plugin - ${plugin.manifest.description}`
-      })), ...themes.map(theme => ({
-        command: theme.entityID,
-        description: `Theme - ${theme.manifest.description}`
-      })) ],
-      header: 'powercord entities list'
+      header: 'powercord entities list',
+      commands: [
+        ...plugins.map(plugin => ({
+          command: plugin.entityID,
+          description: `Plugin - ${plugin.manifest.description}`
+        })),
+        ...themes.map(theme => ({
+          command: theme.entityID,
+          description: `Theme - ${theme.manifest.description}`
+        }))
+      ],
     };
   }
 };
