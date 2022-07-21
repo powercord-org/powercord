0e4aac7cb0461abb82b5ddc10dd7ffd03e5fbdbf
Albert Portnoy <albert@albertp.dev>
2022-07-20 20:24:23 -0500

    Work with plugins and themes

--- src/Powercord/plugins/pc-moduleManager/commands/uninstall.js
+++ src/Powercord/plugins/pc-moduleManager/commands/uninstall.js
@@ -2,73 +2,79 @@ const Modal = require('../components/ConfirmModal');
 const { React } = require('powercord/webpack');
 const { open: openModal, close: closeModal } = require('powercord/modal');
 
+const { resp } = require('../util/resp');
+
 module.exports = {
   command: 'uninstall',
-  description: 'Uninstall a selected plugin and delete the files.',
-  usage: '{c} [ plugin ID ]',
-  executor (args) {
-    let result;
+  description: 'Uninstall a plugin/theme',
+  usage: '{c} [ plugin/theme ID ]',
+  executor ([ id ]) {
+    const isPlugin = powercord.pluginManager.plugins.has(id);
+    const isTheme = powercord.styleManager.themes.has(id);
 
-    if (powercord.pluginManager.plugins.has(args[0])) {
-      if (args[0].startsWith('pc-')) {
-        result = `->> ERROR: You cannot uninstall an internal plugin!
-            (Tried to unload ${args[0]})`;
-      } else {
-        openModal(() => React.createElement(Modal, {
-          red: true,
-          header: `Uninstall ${args[0]}`,
-          desc: `Are you sure you want to uninstall and delete ${args[0]}? This cannot be undone.`,
-          onConfirm: () => {
-            powercord.pluginManager.uninstall(args[0]);
-
-            powercord.api.notices.sendToast(`PDPluginUninstalled-${args[0]}`, {
-              header: 'Plugin Uninstalled',
-              content: `${args[0]} uninstalled and deleted`,
-              type: 'info',
-              timeout: 10e3,
-              buttons: [ {
-                text: 'Got It',
-                color: 'green',
-                size: 'medium',
-                look: 'outlined'
-              } ]
-            });
-          },
-          onCancel: () => closeModal()
-        }));
-      }
-      return {
-        send: false
-      };
+    if (!isPlugin && !isTheme) { // No match
+      return resp(false, `Could not find plugin or theme matching "${id}".`);
+    } else if (isPlugin && isTheme) { // Duplicate name
+      return resp(false, `"${id}" is in use by both a plugin and theme. You will have to uninstall it from settings.`);
+    } else if (isPlugin && id.startsWith('pc-')) { // Internal plugin
+      return resp(false, `"${id}" provides core functionality and cannot be uninstalled.`);
     }
-    result = `->> ERROR: Tried to disable a non-installed plugin!
-          (${args[0]})`;
+
+    const manager = isPlugin ? powercord.pluginManager : powercord.styleManager;
 
 
-    return {
-      send: false,
-      result: `\`\`\`diff\n${result}\`\`\``
-    };
+    openModal(() => React.createElement(Modal, {
+      red: true,
+      header: `Uninstall ${id}`,
+      desc: `Are you sure you want to uninstall and delete ${id}? This cannot be undone.`,
+      onConfirm: () => {
+        manager.uninstall(id);
+
+        powercord.api.notices.sendToast(`PDPluginUninstalled-${id}`, {
+          header: `${isPlugin ? 'Plugin' : 'Theme'} Uninstalled`,
+          content: `${id} uninstalled and deleted`,
+          type: 'info',
+          timeout: 10e3,
+          buttons: [ {
+            text: 'Got It',
+            color: 'green',
+            size: 'medium',
+            look: 'outlined'
+          } ]
+        });
+      },
+      onCancel: () => closeModal()
+    }));
   },
 
   autocomplete (args) {
-    const plugins = powercord.pluginManager.getPlugins()
-      .sort((a, b) => a - b)
-      .map(plugin => powercord.pluginManager.plugins.get(plugin));
+    const plugins = Array.from(powercord.pluginManager.plugins.values())
+      .filter(plugin =>
+        !plugin.entityID.startsWith('pc-') &&
+        plugin.entityID.toLowerCase().includes(args[0]?.toLowerCase())
+      );
+
+    const themes = Array.from(powercord.styleManager.themes.values())
+      .filter(theme =>
+        theme.entityID.toLowerCase().includes(args[0]?.toLowerCase())
+      );
 
     if (args.length > 1) {
       return false;
     }
 
     return {
-      commands: plugins
-        .filter(plugin => plugin.entityID !== 'pc-commands' &&
-          plugin.entityID.toLowerCase().includes(args[0] && args[0].toLowerCase()))
-        .map(plugin => ({
+      header: 'powercord entities list',
+      commands: [
+        ...plugins.map(plugin => ({
           command: plugin.entityID,
-          description: plugin.manifest.description
+          description: `Plugin - ${plugin.manifest.description}`
         })),
-      header: 'powercord plugin list'
+        ...themes.map(theme => ({
+          command: theme.entityID,
+          description: `Theme - ${theme.manifest.description}`
+        }))
+      ]
     };
   }
 };
